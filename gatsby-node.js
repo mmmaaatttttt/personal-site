811d271e01606e0c6970ddba55a86ace6e965f5d
Matt Lane <lane.matthew@gmail.com>
2017-09-30 21:21:18 -0700

    gatsby starter tutorial

---
+++ gatsby-node.js
@@ -0,0 +1,45 @@
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`);
+
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    console.log(node.internal.content);
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    });
+  }
+};
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators;
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      result.data.allMarkdownRemark.edges.map(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            slug: node.fields.slug
+          }
+        });
+      });
+      resolve();
+    });
+  });
+};
