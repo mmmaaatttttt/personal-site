84d72cf3d8d2888dd8ea6be8840e9b901fd1328d
Matt Lane <lane.matthew@gmail.com>
2017-11-07 21:49:21 -0800

    adding more nav styles

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -4,26 +4,41 @@ import Link from "gatsby-link";
 import styled from "styled-components";
 import { rhythm } from "../utils/typography";
 
-const H3 = styled.h3`
-  // margin-bottom: ${rhythm(2)};
-  // display: inline-block;
-  // font-style: normal;
-`;
-
 const StyledNav = styled.nav`
   display: flex;
   justify-content: space-between;
-  align-content: center;
+  align-items: center;
   padding: ${rhythm(0.5)};
+  flex-direction: column;
+  background-color: #f9f9f9;
+  border-bottom: 1px solid #bbb;
+  box-shadow: 0 1px 12px 3px #e2e2e2;
+`;
+
+const StyledHeader = styled.h3`
+  margin-bottom: ${rhythm(0.25)};
+`;
+
+const StyledLinkContainer = styled.div`
+  display: flex;
+  width: 25%;
+  justify-content: center;
+
+  & > * {
+    margin: 0 ${rhythm(0.25)};
+  }
 `;
 
 const Navbar = ({ children, title }) => (
   <StyledNav>
     <Link to="/">
-      <H3>{title}</H3>
+      <StyledHeader>{title}</StyledHeader>
     </Link>
-    <Link to="/about">About</Link>
-    <Link to="/about">Stories</Link>
+    <StyledLinkContainer>
+      <Link to="/about">About</Link>
+      <span>|</span>
+      <Link to="/about">Stories</Link>
+    </StyledLinkContainer>
   </StyledNav>
 );
 
