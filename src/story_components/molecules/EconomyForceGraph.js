68301736488011551c04c06b548fb406c486a161
Matt Lane <lane.matthew@gmail.com>
2017-12-26 22:59:27 -0800

    first pass force simulation (seems slow)

--- src/story_components/molecules/EconomyForceGraph.js
+++ src/story_components/molecules/EconomyForceGraph.js
@@ -1,12 +1,57 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import {
+  forceSimulation,
+  forceLink,
+  forceManyBody,
+  forceCenter
+} from "d3-force";
+import { select } from "d3-selection";
 import COLORS from "../../utils/styles";
 
 class EconomyForceGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: Array.from({ length: this.props.people }, (_, i) => ({
+        id: i,
+        money: 10,
+        cx: this.props.cx,
+        cy: this.props.cy
+      }))
+    };
+    this.handleTick = this.handleTick.bind(this);
+  }
+
+  componentDidMount() {
+    const { cx, cy } = this.props;
+    this.simulation = forceSimulation()
+      .force("charge", forceManyBody())
+      .force("center", forceCenter(cx, cy));
+
+    this.simulation
+      .nodes(this.state.nodes.map(node => ({ ...node })))
+      .on("tick", this.handleTick);
+  }
+
+  handleTick() {
+    const nodes = this.state.nodes.map((node, i) => ({
+      ...node,
+      cx: this.simulation.nodes()[i].x,
+      cy: this.simulation.nodes()[i].y
+    }));
+    this.setState({ nodes });
+  }
+
   render() {
-    const { cx, cy, people } = this.props;
-    const circles = Array.from({ length: people }, (_, i) => (
-      <circle key={i} cx={cx} cy={cy} r={5} fill={COLORS.MAROON} />
+    const circles = this.state.nodes.map((node, i) => (
+      <circle
+        key={i}
+        cx={node.cx}
+        cy={node.cy}
+        r={node.money}
+        fill={COLORS.MAROON}
+      />
     ));
     return <g>{circles}</g>;
   }
