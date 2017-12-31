9796d3d5e198f0f959c4731b40a2ced7d333946b
Matt Lane <lane.matthew@gmail.com>
2017-12-31 14:36:39 -0800

    using react-move for  initialization

--- src/story_components/molecules/EconomyForceGraph.js
+++ src/story_components/molecules/EconomyForceGraph.js
@@ -1,55 +1,56 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {
-  forceSimulation,
-  forceLink,
-  forceManyBody,
-  forceCenter
-} from "d3-force";
-import { select } from "d3-selection";
 import COLORS from "../../utils/styles";
+import NodeGroup from "react-move/NodeGroup";
 
 class EconomyForceGraph extends Component {
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.playing && !this.props.playing) {
-      const { cx, cy, people } = this.props;
-      this.simulation = forceSimulation()
-        .force("charge", forceManyBody())
-        .force("center", forceCenter(cx, cy));
-
-      this.simulation
-        .nodes(this.generateNodes(cx, cy, people))
-        .on("tick", this.forceUpdate.bind(this));
-    }
-  }
-
-  generateNodes(cx, cy, people) {
-    return Array.from({ length: people }, (_, i) => ({
-      id: i,
-      money: 10,
-      cx: cx + Math.cos(i * 2 * Math.PI / people) * cx / 2,
-      cy: cy + Math.sin(i * 2 * Math.PI / people) * cy / 2
-    }));
+  constructor(props) {
+    super(props);
+    this.state = {
+      traders: null
+    };
   }
 
   render() {
     const { cx, cy, people, playing, paused } = this.props;
-
-    // check if there's a simulation first, base coordinates off simulation if it exists
-    const nodes = this.simulation
-      ? this.simulation.nodes()
-      : this.generateNodes(cx, cy, people);
-
-    const circles = nodes.map((node, i) => (
-      <circle
-        key={i}
-        cx={node.x || node.cx}
-        cy={node.y || node.cy}
-        r={node.money}
-        fill={COLORS.MAROON}
-      />
-    ));
-    return <g>{circles}</g>;
+    const radius = cx * 0.7;
+    return (
+      <NodeGroup
+        data={Array.from({ length: people }, (_, i) => ({ id: i }))}
+        keyAccessor={(_, i) => i}
+        start={() => ({
+          theta: 0,
+          color: COLORS.MAROON,
+          money: 10
+        })}
+        enter={(_, i) => ({
+          theta: [(people - i) * 2 * Math.PI / people],
+          timing: { duration: 300 }
+        })}
+        update={(_, i) => ({
+          theta: [(people - i) * 2 * Math.PI / people],
+          timing: { duration: 300 }
+        })}
+        leave={(_, i) => ({
+          theta: [0],
+          timing: { duration: 300 }
+        })}
+      >
+        {nodes => (
+          <g>
+            {nodes.map(({ key, state: { theta, money, color } }) => (
+              <circle
+                key={key}
+                cx={cx + Math.cos(theta) * radius}
+                cy={cy + Math.sin(theta) * radius}
+                fill={color}
+                r={money}
+              />
+            ))}
+          </g>
+        )}
+      </NodeGroup>
+    );
   }
 }
 
