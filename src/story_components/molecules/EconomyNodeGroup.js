e254d2aec71b7d233e3557d001befe741466e6af
Matt Lane <lane.matthew@gmail.com>
2018-01-12 22:54:26 -0800

    basic simulation working with play/pause

--- src/story_components/molecules/EconomyNodeGroup.js
+++ src/story_components/molecules/EconomyNodeGroup.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { forceSimulation } from "d3-force";
 import { forceBounce } from "d3-force-bounce";
 import { forceSurface } from "d3-force-surface";
+import { select } from "d3-selection";
 import COLORS from "../../utils/styles";
 
 class EconomyNodeGroup extends Component {
@@ -11,7 +12,7 @@ class EconomyNodeGroup extends Component {
     this.simulation = forceSimulation()
       .alphaDecay(0)
       .velocityDecay(0)
-      .force("bounce", forceBounce().radius(node => node.radius))
+      .force("bounce", forceBounce().radius(node => node.r))
       .force(
         "surface",
         forceSurface()
@@ -22,9 +23,8 @@ class EconomyNodeGroup extends Component {
             { from: { x: width, y: 0 }, to: { x: 0, y: 0 } }
           ])
           .oneWay(true)
-          .radius(node => node.radius)
-      )
-      .on("tick", this.updateNodes);
+          .radius(node => node.r)
+      );
 
     this.simulation
       .nodes(this.generateNodes(this.props))
@@ -59,27 +59,70 @@ class EconomyNodeGroup extends Component {
         )
       );
       const node = { x, y, vx, vy, r };
-      if (!playing || paused) {
-        node.fx = node.x;
-        node.fy = node.y;
-      }
       newNodes.push(node);
     }
     return newNodes;
   }
 
-  updateNodes() {}
+  updateNodes = () => {
+    const { playing, paused } = this.props;
+    const shouldMove = playing && !paused;
+    this.simulation.nodes().forEach(node => {
+      if (shouldMove) {
+        node.fx = null;
+        node.fy = null;
+        node.vx = node.vx || node.lastVx;
+        node.vy = node.vy || node.lastVy;
+        node.lastVx = null;
+        node.lastVy = null;
+        this.simulation.restart();
+      } else {
+        node.lastVx = node.lastVx || node.vx;
+        node.lastVy = node.lastVy || node.vy;
+        node.fx = node.x;
+        node.fy = node.y;
+      }
+    });
+    console.log(this.simulation.nodes()[0]);
+
+    let nodes = select(this.g)
+      .selectAll("circle")
+      .data(this.simulation.nodes().map(this.forceInCanvas));
+
+    nodes.exit().remove();
+
+    nodes
+      .enter()
+      .append("circle")
+      .attr("r", d => d.r)
+      .attr("fill", COLORS.MAROON)
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
+
+    if (shouldMove) {
+      nodes.attr("cx", d => d.x).attr("cy", d => d.y);
+    }
+
+    this.forceUpdate();
+  };
+
+  forceInCanvas = node => {
+    const { max, min } = Math;
+    const { width, height } = this.props;
+    node.x = max(node.r, min(width - node.r, node.x));
+    node.y = max(node.r, min(height - node.r, node.y));
+    return node;
+  };
 
   render() {
     const { people, playing, paused } = this.props;
 
-    const nodes = this.simulation.nodes();
+    const nodes = select(this.g)
+      .selectAll("circle")
+      .data();
 
     // look back at this: https://bl.ocks.org/vasturiano/2992bcb530bc2d64519c5b25201492fd
-    const circles = nodes.map((node, i) => (
-      <circle key={i} cx={node.x} cy={node.y} r={node.r} fill={COLORS.MAROON} />
-    ));
-    return <g>{circles}</g>;
+    return <g ref={g => (this.g = g)} />;
   }
 }
 
