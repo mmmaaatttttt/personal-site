5864e8f006c8662ca1d7584ea0b3fe630689e375
Matt Lane <lane.matthew@gmail.com>
2018-01-28 10:05:50 -0800

    refactoring collision handlers

--- src/data/income-inequality.js
+++ src/data/income-inequality.js
@@ -1,14 +1,12 @@
 import { euclideanDistance } from "../utils/mathHelpers";
 
-const normalCollision = (speeds, multiplier, nodes) => {
-  const newSpeeds = [...speeds];
-  nodes.forEach(node => {
-    newSpeeds[node.i] = euclideanDistance(node.vx, node.vy) / multiplier;
-  });
-  return newSpeeds;
-};
+const normalCollision = (speeds, multiplier, nodes) =>
+  _handleCollision(speeds, multiplier, nodes, _updateHandlerNormal);
 
-const collisionMaximizedByLeastWealth = (speeds, multiplier, nodes) => {
+const collisionMaximizedByLeastWealth = (speeds, multiplier, nodes) =>
+  _handleCollision(speeds, multiplier, nodes, _updateHandlerLeastWealth);
+
+const _handleCollision = (speeds, multiplier, nodes, updateHandler) => {
   const originalEnergies = nodes.map(node => speeds[node.i] ** 2);
   const newEnergies = nodes.map(
     node => (euclideanDistance(node.vx, node.vy) / multiplier) ** 2
@@ -18,11 +16,7 @@ const collisionMaximizedByLeastWealth = (speeds, multiplier, nodes) => {
   if (newEnergies[0] === 0 && newEnergies[1] === 0) return speeds;
 
   const epsilon = newEnergies[0] / (originalEnergies[0] + originalEnergies[1]);
-  const newDelta = epsilon * Math.min(...originalEnergies);
-  const updatedNewEnergies =
-    Math.random() < 0.5
-      ? [originalEnergies[0] + newDelta, originalEnergies[1] - newDelta]
-      : [originalEnergies[0] - newDelta, originalEnergies[1] + newDelta];
+  const updatedNewEnergies = updateHandler(epsilon, originalEnergies);
   const newSpeeds = [...speeds];
   nodes.forEach((node, idx) => {
     const scaleFactor = (updatedNewEnergies[idx] / newEnergies[idx]) ** (1 / 2);
@@ -30,8 +24,24 @@ const collisionMaximizedByLeastWealth = (speeds, multiplier, nodes) => {
     node.vy *= scaleFactor;
     newSpeeds[node.i] = euclideanDistance(node.vx, node.vy) / multiplier;
   });
-
   return newSpeeds;
+};
+
+const _updateHandlerNormal = (eps, energies) => {
+  if (Math.random() < 0.5) {
+    const delta = eps * energies[1];
+    return [energies[0] + delta, energies[1] - delta];
+  } else {
+    const delta = eps * energies[0];
+    return [energies[0] - delta, energies[1] + delta];
+  }
+};
+
+const _updateHandlerLeastWealth = (eps, energies) => {
+  const delta = eps * Math.min(...energies);
+  return Math.random() < 0.5
+    ? [energies[0] + delta, energies[1] - delta]
+    : [energies[0] - delta, energies[1] + delta];
 };
 
 const updateSpeeds = [normalCollision, collisionMaximizedByLeastWealth];
