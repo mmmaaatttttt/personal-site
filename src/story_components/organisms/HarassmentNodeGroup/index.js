f17c808fa3b8cdc04906b9b6e6a0a39fc73b66c4
Matt Lane <lane.matthew@gmail.com>
2018-04-03 21:21:30 -0700

    adding wave functionality
    also adding placeholders to keep track of how much harassment each group receives.
    need to refactor into a visible UI component.

--- src/story_components/organisms/HarassmentNodeGroup/index.js
+++ src/story_components/organisms/HarassmentNodeGroup/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { select } from "d3-selection";
+import { select, selectAll } from "d3-selection";
 import { transition } from "d3-transition";
-import { euclideanDistance } from "utils/mathHelpers";
 import { interval } from "d3-timer";
+import { euclideanDistance } from "utils/mathHelpers";
 import {
   initializeSimulation,
   generateSimulationNodes,
@@ -12,11 +12,18 @@ import {
 import COLORS from "utils/styles";
 
 class HarassmentNodeGroup extends Component {
+  state = { shoutCount: 0 };
+
   componentWillMount() {
     const { width, height } = this.props;
-    this.simulation = initializeSimulation(width, height);
+    this.simulation = initializeSimulation(width, height, this.handleCollision);
     this.generateNodes(this.props);
     this.simulation.on("tick", () => this.updateNodes(this.props));
+    // DELETE THIS
+    this.shoutsHeard = {
+      [COLORS.BLUE]: new Set(),
+      [COLORS.MAROON]: new Set()
+    };
   }
 
   generateNodes = props => {
@@ -43,39 +50,94 @@ class HarassmentNodeGroup extends Component {
 
   updateNodes = props => {
     const { velocityMultiplier, initialV, blueCount } = props;
-    const isMoving = this.props.playing && !this.props.paused;
     const scaledInitialSpeed = initialV * velocityMultiplier;
     const colorFn = d => d.properties.color;
-    updateSimulationNodes(this.simulation, this.g, colorFn, isMoving);
+    updateSimulationNodes(this.simulation, this.g, colorFn, this.isMoving());
     let test = Math.random();
-    if (test < 0.01 && isMoving) {
-      const nodes = this.simulation.nodes();
-      const randomIdx = Math.floor(nodes.length * Math.random());
-      const soundWave = interval(() => {
-        soundWave.__calledCount++;
-        if (soundWave.__calledCount <= 5) {
-          const randomNode = nodes[randomIdx];
-          // prettier-ignore
-          select(this.g)
-            .insert("circle", "circle")
-            .attr("cx", randomNode.x)
-            .attr("cy", randomNode.y)
-            .attr("r", randomNode.r * 2)
-            .attr("fill", "none")
-            .attr("stroke", COLORS.RED)
-            .classed("shout", true)
-          .transition()
-            .duration(2000)
-            .ease(Math.sqrt)
-            .attr("r", randomNode.r * 5)
-            .style("stroke-opacity", 1e-6)
-            .remove();
-        } else {
-          soundWave.stop();
+    this.__checkIntersections();
+  };
+
+  isMoving = () => {
+    return this.props.playing && !this.props.paused;
+  };
+
+  handleCollision = (...nodes) => {
+    const sameColor = nodes[0].properties.color === nodes[1].properties.color;
+    if (sameColor && this.isMoving()) {
+      nodes.forEach(node => {
+        let test = Math.random();
+        if (test < 0.1) {
+          this.setState(
+            { shoutCount: this.state.shoutCount + 1 },
+            this.__generateWave(node)
+          );
         }
-      }, 100);
-      soundWave.__calledCount = 0;
+      });
     }
+  };
+
+  __checkIntersections = () => {
+    selectAll(".shout").each((d, i, hmm) => {
+      const color = d.nodeKey.split("-")[0];
+      const waveCircle = select(hmm[i]);
+      const waveX = +waveCircle.attr("cx");
+      const waveY = +waveCircle.attr("cy");
+      const waveR = +waveCircle.attr("r");
+      this.simulation.nodes().forEach(node => {
+        const nodeColor = node.properties.color;
+        const { x, y, r } = node;
+        const waveDistance = euclideanDistance(x - waveX, y - waveY);
+        if (nodeColor !== color && waveDistance < r + waveR) {
+          // DELETE THIS
+          const blueSize = this.shoutsHeard[COLORS.BLUE].size;
+          const brownSize = this.shoutsHeard[COLORS.MAROON].size;
+          // this.shoutsHeard[nodeColor].add(`${node.key}:${d.shoutCount}`);
+          this.shoutsHeard[nodeColor].add(d.shoutCount);
+          if (
+            this.shoutsHeard[COLORS.BLUE].size > blueSize ||
+            this.shoutsHeard[COLORS.MAROON].size > brownSize
+          ) {
+            // console.log("BLUE RECEIVED", this.shoutsHeard[COLORS.BLUE].size);
+            // console.log("BROWN RECEIVED", this.shoutsHeard[COLORS.MAROON].size);
+            console.log(
+              // "RATIO",
+              this.shoutsHeard[COLORS.BLUE].size /
+                (this.shoutsHeard[COLORS.MAROON].size || 1)
+            );
+          }
+        }
+      }, this);
+    });
+  };
+
+  __generateWave = node => {
+    const { shoutCount } = this.state;
+    const soundWave = interval(() => {
+      soundWave.__calledCount++;
+      if (soundWave.__calledCount <= 5) {
+        // prettier-ignore
+        select(this.g)
+          .insert("circle", "circle")
+          .attr("cx", node.x)
+          .attr("cy", node.y)
+          .attr("r", node.r * 2)
+          .attr("fill", COLORS.RED)
+          .attr("fill-opacity", 0.75)
+          .attr("stroke", COLORS.RED)
+          .classed("shout", true)
+          .datum({ shoutCount, nodeKey: node.key })
+        .transition()
+          .duration(2000)
+          .ease(Math.sqrt)
+          .attr("r", node.r * 10)
+          .style("stroke-opacity", 1e-6)
+          .style("fill-opacity", 1e-6)
+          .remove();
+      } else {
+        soundWave.stop();
+      }
+    }, 200);
+    soundWave.__calledCount = 0;
   };
 
   componentWillUpdate(nextProps) {
