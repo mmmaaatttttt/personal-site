7220ef3a25a68592302d506ad9cabe40ae679426
Matt Lane <lane.matthew@gmail.com>
2018-04-30 07:48:45 -0700

    adding enzyme

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
     "^.+\\.jsx?$": "<rootDir>/test/transformer.js"
   },
   testPathIgnorePatterns: ["/node_modules/", "<rootDir>/.cache/"],
+  setupTestFrameworkScriptFile: "<rootDir>test/jest-setup.js",
+  snapshotSerializers: ["enzyme-to-json/serializer"],
   moduleFileExtensions: ["jsx", "js"],
   collectCoverage: true,
   coverageReporters: ["lcov", "text", "html"]
