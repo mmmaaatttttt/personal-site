18915391412c60652adfbed9d635c71ddcc7c191
Matt Lane <lane.matthew@gmail.com>
2018-04-29 22:17:08 -0700

    adding math unit tests

--- src/utils/mathHelpers.js
+++ src/utils/mathHelpers.js
@@ -27,23 +27,31 @@ function generateData(
 }
 
 function choices(arr, num) {
-  const values = new Set();
-  while (values.size < num) {
-    const randomIdx = Math.floor(Math.random() * arr.length);
-    values.add(arr[randomIdx]);
-  }
-  return Array.from(values);
+  return _shuffle(arr.slice()).slice(0, num);
 }
 
 function euclideanDistance(...pts) {
   return pts.reduce((sum, pt) => sum + pt ** 2, 0) ** (1 / 2) || 0;
 }
 
-const average = (nums, accessor) => {
+function average(nums, accessor) {
   if (!nums.length) return 0;
   if (!accessor) accessor = num => num;
   const total = nums.reduce((sum, cur) => sum + accessor(cur), 0);
   return total / nums.length;
-};
+}
+
+function _swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+  return arr;
+}
+
+function _shuffle(arr) {
+  for (var i = arr.length - 1; i >= 0; i--) {
+    var randomIdx = Math.floor(Math.random() * i);
+    _swap(arr, i, randomIdx);
+  }
+  return arr;
+}
 
 export { generateData, choices, euclideanDistance, average };
