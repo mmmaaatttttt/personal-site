b851cfc9a4b3af5fcfbaf68aed33c47ef13692d6
Matt Lane <lane.matthew@gmail.com>
2018-05-19 10:59:09 -0700

    adding new string helper

--- src/utils/stringHelpers.js
+++ src/utils/stringHelpers.js
@@ -1,3 +1,5 @@
 const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+const camelCaseToTitle = str =>
+  capitalize(str.replace(/[A-Z]/g, c => ` ${c.toUpperCase()}`));
 
-export { capitalize };
+export { capitalize, camelCaseToTitle };
