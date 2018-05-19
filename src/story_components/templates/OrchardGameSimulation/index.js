538f459b8e72535632122d817f3263090cee7e1d
Matt Lane <lane.matthew@gmail.com>
2018-05-19 10:59:28 -0700

    creating orchard game simulation visualization

--- src/story_components/templates/OrchardGameSimulation/index.js
+++ src/story_components/templates/OrchardGameSimulation/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Button } from "story_components";
+import {
+  Button,
+  FlexContainer,
+  HorizontalBar,
+  NarrowContainer
+} from "story_components";
 import withCaption from "hocs/withCaption";
 import COLORS from "utils/styles";
+import { camelCaseToTitle } from "utils/stringHelpers";
 
 class OrchardGameSimulation extends Component {
   state = {
@@ -13,17 +19,27 @@ class OrchardGameSimulation extends Component {
     }))
   };
 
+  resetSimulation = () => {
+    this.setState({
+      playing: false,
+      playData: this.props.strategies.map(() => ({
+        gamesPlayed: 0,
+        gamesWon: 0
+      }))
+    });
+  };
+
   togglePlaying = () => {
     this.setState(
       prevState => ({ playing: !prevState.playing }),
       () => {
         if (this.timerId) return this.clearTimer();
-        const { strategies } = this.props;
+        const { strategies, wildCardCount } = this.props;
         this.timerId = setInterval(() => {
           strategies.forEach((strategy, i) => {
             this.setState(prevState => {
               const playData = [...prevState.playData];
-              const isWin = +this.simulateGame(strategy);
+              const isWin = +this.simulateGame(strategy, wildCardCount);
               playData[i] = {
                 gamesPlayed: playData[i].gamesPlayed + 1,
                 gamesWon: playData[i].gamesWon + isWin
@@ -31,7 +47,7 @@ class OrchardGameSimulation extends Component {
               return { playData };
             });
           });
-        }, 10);
+        }, 20);
       }
     );
   };
@@ -49,17 +65,22 @@ class OrchardGameSimulation extends Component {
 
   gameLost = ravenCount => ravenCount === 0;
 
-  simulateGame = strategy => {
-    let fruitCounts = [4, 4, 4, 4];
-    let ravenCount = 5;
+  simulateGame = (strategy, wildCardCount) => {
+    let fruitCounts = [...this.props.fruitCounts];
+    let { ravenCount } = this.props;
     while (true) {
       const idx = Math.floor((fruitCounts.length + 2) * Math.random());
       if (idx < fruitCounts.length)
         fruitCounts[idx] = Math.max(fruitCounts[idx] - 1, 0);
       else if (idx === fruitCounts.length) ravenCount--;
       else {
-        const strategicIdx = strategy(fruitCounts);
-        fruitCounts[strategicIdx] = Math.max(fruitCounts[strategicIdx] - 1, 0);
+        for (var i = 0; i < wildCardCount; i++) {
+          const strategicIdx = strategy(fruitCounts);
+          fruitCounts[strategicIdx] = Math.max(
+            fruitCounts[strategicIdx] - 1,
+            0
+          );
+        }
       }
       if (this.gameWon(fruitCounts)) return true;
       if (this.gameLost(ravenCount)) return false;
@@ -68,25 +89,65 @@ class OrchardGameSimulation extends Component {
 
   render() {
     const { playing, playData } = this.state;
+    const { strategies } = this.props;
     return (
-      <div>
-        <Button onClick={this.togglePlaying}>
-          {playing ? "Pause" : "Play"}
-        </Button>
-        {playData.map((d, i) => (
-          <h1 key={i}>
-            {this.props.strategies[i].name}: {d.gamesWon} / {d.gamesPlayed} ≈{" "}
-            {(d.gamesWon / d.gamesPlayed * 100).toFixed(2)}%
-          </h1>
-        ))}
-      </div>
+      <NarrowContainer width="80%" fullWidthAt="small">
+        <FlexContainer main="center">
+          <Button onClick={this.togglePlaying}>
+            {playing ? "Pause" : "Play"}
+          </Button>
+          {!playing ? (
+            <Button onClick={this.resetSimulation} color={COLORS.RED}>
+              Reset Simulation
+            </Button>
+          ) : null}
+        </FlexContainer>
+        {playData.map((d, i) => {
+          const percentage =
+            (d.gamesWon / d.gamesPlayed * 100 || 0).toFixed(1) + "%";
+          const data = [
+            {
+              size: d.gamesWon,
+              color: COLORS.GREEN,
+              tooltipText: `Games Won: ${d.gamesWon.toLocaleString()}`
+            },
+            {
+              size: d.gamesPlayed - d.gamesWon,
+              color: COLORS.RED,
+              tooltipText: `Games Played: ${d.gamesPlayed.toLocaleString()}`
+            },
+            {
+              size: 0,
+              color: COLORS.GRAY,
+              tooltipText: `Win Percentage: ${percentage}`
+            }
+          ];
+          return (
+            <HorizontalBar
+              key={i}
+              title={`${camelCaseToTitle(
+                strategies[i].name
+              )} Strategy: ${percentage}`}
+              data={data}
+            />
+          );
+        })}
+      </NarrowContainer>
     );
   }
 }
 
+OrchardGameSimulation.propTypes = {
+  fruitCounts: PropTypes.arrayOf(PropTypes.number).isRequired,
+  ravenCount: PropTypes.number.isRequired,
+  wildCardCount: PropTypes.number.isRequired,
+  strategies: PropTypes.arrayOf(PropTypes.func).isRequired
+};
+
 OrchardGameSimulation.defaultProps = {
   fruitCounts: [4, 4, 4, 4],
   ravenCount: 5,
+  wildCardCount: 1,
   strategies: [
     function mostPlentiful(fruitCounts) {
       const maxCount = Math.max(...fruitCounts);
@@ -104,7 +165,7 @@ OrchardGameSimulation.defaultProps = {
       const randomIndex = Math.floor(Math.random() * validIndices.length);
       return validIndices[randomIndex];
     },
-    function favorite(fruitCounts) {
+    function favoriteColor(fruitCounts) {
       return fruitCounts.findIndex(c => c > 0);
     }
   ]
