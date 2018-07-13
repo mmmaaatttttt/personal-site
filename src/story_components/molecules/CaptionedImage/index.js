8f8efdfc00da398861969dd6816a74c132297dab
Matt Lane <lane.matthew@gmail.com>
2018-07-13 14:23:55 -0700

    making sure images are centered

--- src/story_components/molecules/CaptionedImage/index.js
+++ src/story_components/molecules/CaptionedImage/index.js
@@ -3,13 +3,14 @@ import styled from "styled-components";
 import { NarrowContainer, Caption } from "story_components";
 import images from "utils/images";
 
-const NoMarginImg = styled.img`
-  margin-bottom: 0;
+const MarginImg = styled.img`
+  display: block;
+  margin: 0 auto;
 `;
 
 const CaptionedImage = ({ width, src, caption }) => (
   <NarrowContainer width={width}>
-    <NoMarginImg src={images[src]} alt={caption} />
+    <MarginImg src={images[src]} alt={caption} />
     <Caption>{caption}</Caption>
   </NarrowContainer>
 );
