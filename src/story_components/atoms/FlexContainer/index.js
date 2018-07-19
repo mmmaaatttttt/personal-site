c63a4c37bb580235564440f3e306fa7ec30e6ae0
Matt Lane <lane.matthew@gmail.com>
2018-07-18 18:29:22 -0700

    adding flex prop to FlexContainer

--- src/story_components/atoms/FlexContainer/index.js
+++ src/story_components/atoms/FlexContainer/index.js
@@ -6,7 +6,7 @@ const cross = props => (props.column ? "justify-content" : "align-items");
 
 const FlexContainer = styled.div`
   display: flex;
-  flex: 1;
+  flex: ${props => props.flex};
   flex-wrap: ${props => (props.wrap ? "wrap" : "nowrap")};
     ${props =>
       props.column &&
@@ -29,15 +29,17 @@ const flexAlignments = [
 
 FlexContainer.propTypes = {
   column: PropTypes.bool.isRequired,
-  main: PropTypes.oneOf(flexAlignments).isRequired,
   cross: PropTypes.oneOf(flexAlignments).isRequired,
+  flex: PropTypes.number.isRequired,
+  main: PropTypes.oneOf(flexAlignments).isRequired,
   wrap: PropTypes.bool.isRequired
 };
 
 FlexContainer.defaultProps = {
-  main: "stretch",
-  cross: "stretch",
   column: false,
+  cross: "stretch",
+  flex: 1,
+  main: "stretch",
   wrap: false
 };
 
