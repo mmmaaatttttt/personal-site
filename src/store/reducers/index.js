1e42717357d7e688e8c092d5f2a35cb4db3fc80f
Matt Lane <lane.matthew@gmail.com>
2018-08-03 20:16:54 -0700

    cleaning up reducers index

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,9 +3,13 @@ const req = require.context(".", true, /\.js$/);
 const combinedReducerObj = req
   .keys()
   .filter(key => key !== "./index.js")
-  .reduce((obj, key) => ({ ...obj, [key]: req(key) }), {});
+  .reduce((obj, key) => ({ ...obj, [key.match(/[^\.\/]+/)[0]]: req(key) }), {});
 
 const rootReducer = combineReducers(combinedReducerObj);
 
-const createStore = () => reduxCreateStore(rootReducer, {});
+const createStore = () =>
+  reduxCreateStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 export default createStore;
