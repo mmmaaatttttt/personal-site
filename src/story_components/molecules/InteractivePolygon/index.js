e84acf57691b808887a19f4553057b4a1929a59d
Matt Lane <lane.matthew@gmail.com>
2018-10-11 20:14:58 -0700

    adding points, lines, and polygon

--- src/story_components/molecules/InteractivePolygon/index.js
+++ src/story_components/molecules/InteractivePolygon/index.js
@@ -3,18 +3,72 @@ import PropTypes from "prop-types";
 
 class InteractivePolygon extends Component {
   state = {
-    sides: this.props.initialSides
+    points: Array.from({ length: this.props.initialSides }, (_, i) => {
+      const { cx, cy, initialSides } = this.props;
+      const angle = (2 * Math.PI * i) / initialSides - Math.PI / 2;
+      const distance = 100;
+      return {
+        id: i,
+        x: cx + distance * Math.cos(angle),
+        y: cy + distance * Math.sin(angle)
+      };
+    }),
+    nextId: this.props.initialSides + 1
+  };
+
+  getPointsString = () => {
+    return this.state.points
+      .reduce((str, obj) => `${str} ${obj.x},${obj.y}`, "")
+      .trim();
+  };
+
+  getLength = () => {
+    return this.polygon.getTotalLength();
+  }
+
+  getCenter = () => {
+    let box = this.polygon.getBBox();
+    return {
+      x: box.x + box.width / 2,
+      y: box.y + box.height / 2
+    }
   }
 
   render() {
-    return null
+    const { points } = this.state;
+    const radius = 8;
+    let circles = points.map(point => (
+      <circle cx={point.x} cy={point.y} r={radius} key={point.id} />
+    ));
+    let lines = points.map((point, i) => {
+      let nextPoint = points[(i + 1) % points.length];
+      return (
+        <line
+          x1={point.x}
+          x2={nextPoint.x}
+          y1={point.y}
+          y2={nextPoint.y}
+          stroke="black"
+          strokeWidth="3"
+          key={`${point.id}-${nextPoint.id}`}
+        />
+      );
+    });
+    let polygon = <polygon points={this.getPointsString()} fill="red" ref={polygon => this.polygon = polygon}/>
+    return (
+      <g>
+        {polygon}
+        {lines}
+        {circles}
+      </g>
+    );
   }
 }
 
 InteractivePolygon.propTypes = {
   cx: PropTypes.number.isRequired,
   cy: PropTypes.number.isRequired,
-  initialSides: PropTypes.number.isRequired,
+  initialSides: PropTypes.number.isRequired
 };
 
 InteractivePolygon.defaultProps = {
--- src/story_components/organisms/IsoperimetricExplorer/index.js
+++ src/story_components/organisms/IsoperimetricExplorer/index.js
@@ -6,8 +6,7 @@ class IsoperimetricExplorer extends Component {
   render() {
     const { width, height } = this.props;
     return (
-      <ClippedSVG width={width} height={height}>
-        <rect x="100" y="100" width="100" height="200" />
+      <ClippedSVG width={width} height={height} id="isoperimetric-svg">
         <InteractivePolygon cx={width / 2} cy={height / 2} initialSides={3} />
       </ClippedSVG>
     );
