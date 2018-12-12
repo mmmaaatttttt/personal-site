6bf7c2eceb0e7ece8e5a3711a7ae17dbd966ed7b
Matt Lane <lane.matthew@gmail.com>
2018-12-12 07:10:55 -0800

    adding margin prop to styledtable

--- src/story_components/atoms/StyledTable/index.js
+++ src/story_components/atoms/StyledTable/index.js
@@ -1,11 +1,22 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const StyledTable = styled.table`
+  margin: ${props => props.margin};
+
   th,
   td {
     text-align: center;
     padding: 0.5rem 0;
   }
 `;
+
+StyledTable.propTypes = {
+  margin: PropTypes.string.isRequired
+};
+
+StyledTable.defaultProps = {
+  margin: "0 0 1.44rem 0"
+}
 
 export default StyledTable;
