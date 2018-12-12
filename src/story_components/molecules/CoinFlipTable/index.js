89048d9857151d25ed8ee3fae650c99113ef1d38
Matt Lane <lane.matthew@gmail.com>
2018-12-12 07:11:10 -0800

    adding coinflip table interactive

---
+++ src/story_components/molecules/CoinFlipTable/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { LabeledSlider, StyledTable } from "story_components";
+import withCaption from "hocs/withCaption";
+import COLORS from "utils/styles";
+
+class CoinFlipTable extends Component {
+  state = {
+    headsProb: 0.5
+  };
+
+  handleProbChange = val => {
+    this.setState({ headsProb: val });
+  };
+
+  format = (probability, dec=0) => `${(probability * 100).toFixed(dec)}%`;
+
+  render() {
+    const { headsProb } = this.state;
+    const tailsProb = 1 - headsProb;
+    const pairProb = headsProb * tailsProb;
+    const formatted = {
+      heads: this.format(headsProb),
+      tails: this.format(tailsProb),
+      pair: this.format(pairProb, 2)
+    };
+    return (
+      <div>
+        <LabeledSlider
+          min={0.01}
+          max={0.99}
+          step={0.01}
+          value={headsProb}
+          handleValueChange={this.handleProbChange}
+          title={`Probability of flipping heads: ${formatted.heads}`}
+          color={COLORS.GREEN}
+          minIcon="times-circle"
+          maxIcon="check-circle"
+        />
+        <StyledTable margin="0.72rem 0 0 0">
+          <thead>
+            <tr>
+              <th>Prob. of H</th>
+              <th>Prob. of T</th>
+              <th>Prob. of HT</th>
+              <th>Prob. of TH</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{formatted.heads}</td>
+              <td>{formatted.tails}</td>
+              <td>{formatted.heads} &times; {formatted.tails} = {formatted.pair}</td>
+              <td>{formatted.tails} &times; {formatted.heads} = {formatted.pair}</td>
+            </tr>
+          </tbody>
+        </StyledTable>
+      </div>
+    );
+  }
+}
+
+CoinFlipTable.propTypes = {};
+
+CoinFlipTable.defaultProps = {};
+
+export default withCaption(CoinFlipTable);
