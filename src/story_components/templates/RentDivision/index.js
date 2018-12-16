dcf062986ad2656ac377324946b43ff8d788f662
Matt Lane <lane.matthew@gmail.com>
2018-12-15 21:27:34 -0800

    starting on Sperner's Lemma interactive

--- src/story_components/templates/RentDivision/index.js
+++ src/story_components/templates/RentDivision/index.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { ColumnLayout, ClippedSVG, Tooltip} from "story_components";
+import { ClippedSVG, LabeledCircle, Tooltip } from "story_components";
 import COLORS from "utils/styles";
 
 class RentDivision extends Component {
-  state = {
-
+  constructor(props) {
+    super(props);
+    const { height, width } = props;
+    const r = height / 2 - 20;
+    let xBase = width / 2;
+    let yBase = height / 2 + 70;
+    this.state = {
+      points: [
+        {
+          x: xBase + r * Math.cos(Math.PI / 2),
+          y: yBase - r * Math.sin(Math.PI / 2),
+          color: COLORS.ORANGE,
+          label: "A"
+        },
+        {
+          x: xBase + r * Math.cos(Math.PI / 2 + (2 * Math.PI) / 3),
+          y: yBase - r * Math.sin(Math.PI / 2 + (2 * Math.PI) / 3),
+          color: COLORS.ORANGE,
+          label: "A"
+        },
+        {
+          x: xBase + r * Math.cos(Math.PI / 2 + (4 * Math.PI) / 3),
+          y: yBase - r * Math.sin(Math.PI / 2 + (4 * Math.PI) / 3),
+          color: COLORS.ORANGE,
+          label: "A"
+        }
+      ]
+    };
   }
 
   render() {
-    return <ColumnLayout>
-      <ClippedSVG width="600" height="600" id="rent">
-
+    const { width, height } = this.props;
+    const labeledCircles = this.state.points.map(p => (
+      <LabeledCircle {...p} key={`${p.x}|${p.y}`} />
+    ));
+    return (
+      <ClippedSVG width={width} height={height} id="rent">
+        {labeledCircles}
       </ClippedSVG>
-    </ColumnLayout>
+    );
   }
 }
 
@@ -21,12 +51,16 @@ RentDivision.propTypes = {
   rent: PropTypes.number.isRequired,
   roomColors: PropTypes.arrayOf(PropTypes.string).isRequired,
   names: PropTypes.arrayOf(PropTypes.string).isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired
 };
 
 RentDivision.defaultProps = {
   rent: 2400,
   roomColors: [COLORS.ORANGE, COLORS.PURPLE, COLORS.GREEN],
-  names: ["Alex", "Brett", "Cameron"]
+  names: ["Alex", "Brett", "Cameron"],
+  width: 600,
+  height: 600
 };
 
 export default RentDivision;
