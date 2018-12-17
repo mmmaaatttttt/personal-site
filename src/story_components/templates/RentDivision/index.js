8499bb41be23ccf9c478d7f1ab3e76e2f2bc994b
Matt Lane <lane.matthew@gmail.com>
2018-12-16 20:32:09 -0800

    can convert between coordinates and rent prices

--- src/story_components/templates/RentDivision/index.js
+++ src/story_components/templates/RentDivision/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { ClippedSVG, LabeledCircle, Polygon, Tooltip } from "story_components";
+import { euclideanDistance, total } from "utils/mathHelpers";
 import COLORS from "utils/styles";
 
 class RentDivision extends Component {
@@ -34,15 +35,35 @@ class RentDivision extends Component {
     };
   }
 
+  get corners() {
+    return this.state.points.slice(0, 3).map(p => ({ x: p.x, y: p.y }));
+  }
+
+  /**
+   * Converts a point's x, y coordinates into a triplet
+   * corresponding to rend prices at that point
+   * @param {Object} point - Coordinates of the point
+   * @param {number} point.x - x-coord of the point
+   * @param {number} point.y - y-coord of the point
+   */
+  getPrices = point => {
+    const { rent } = this.props;
+    let distances = this.corners.map(c =>
+      euclideanDistance(point.x - c.x, point.y - c.y)
+    );
+    let totalDistance = total(distances);
+    return distances.map(d => d * rent / totalDistance);
+  }
+
   render() {
     const { width, height } = this.props;
-    const { points } = this.state
-    const labeledCircles = this.state.points.map(p => (
+    const { points } = this.state;
+    const labeledCircles = points.map(p => (
       <LabeledCircle {...p} key={`${p.x}|${p.y}`} />
     ));
     return (
       <ClippedSVG width={width} height={height} id="rent">
-        <Polygon points={points.slice(0,3)} fill="none" />
+        <Polygon points={this.corners} fill="none" />
         {labeledCircles}
       </ClippedSVG>
     );
