c328ac9efa66ecc4f831efb84e230f2216ab98e5
Matt Lane <lane.matthew@gmail.com>
2018-12-27 20:27:13 -0800

    major rewrite of RentDivision component, points are messed up though

--- src/story_components/templates/RentDivision/index.js
+++ src/story_components/templates/RentDivision/index.js
@@ -5,53 +5,148 @@ import {
   ColumnLayout,
   FlexContainer,
   LabeledCircle,
-  Polygon,
   RadioButtonGroup,
   Tooltip
 } from "story_components";
-import { euclideanDistance, total } from "utils/mathHelpers";
 import COLORS from "utils/styles";
+import { interpolate } from "utils/mathHelpers";
 
 class RentDivision extends Component {
   constructor(props) {
     super(props);
-    const { height, width, rent } = props;
-    const r = height / 2 - 20;
-    let xBase = width / 2;
-    let yBase = height / 2 + 70;
     this.state = {
-      activePtIdx: 0,
+      activePtLoc: [0, 0],
       currentColorIdx: null,
       tooltipVisible: false,
       tooltipX: 0,
       tooltipY: 0,
       tooltipBody: "",
-      points: [
-        {
-          x: xBase + r * Math.cos(Math.PI / 2),
-          y: yBase - r * Math.sin(Math.PI / 2),
-          color: COLORS.BLACK,
-          label: "A",
-          prices: [0, 0, rent]
-        },
-        {
-          x: xBase + r * Math.cos(Math.PI / 2 + (2 * Math.PI) / 3),
-          y: yBase - r * Math.sin(Math.PI / 2 + (2 * Math.PI) / 3),
-          color: COLORS.BLACK,
-          label: "B",
-          prices: [rent, 0, 0]
-        },
-        {
-          x: xBase + r * Math.cos(Math.PI / 2 + (4 * Math.PI) / 3),
-          y: yBase - r * Math.sin(Math.PI / 2 + (4 * Math.PI) / 3),
-          color: COLORS.BLACK,
-          label: "C",
-          prices: [0, rent, 0]
-        }
-      ]
+      points: this.generateAllPoints()
     };
-    this.state.triangles = [this.state.points];
   }
+
+  /**
+   * Generates data on every row of points
+   * in the triangulation. For example, given the following diagram:
+   *
+   *         1
+   *       /   \
+   *      2 --- 3
+   *    /   \ /   \
+   *   4 --- 5 --- 6
+   *
+   * it should generate an array of the form
+   * [
+   *   [ { 1 data } ],
+   *   [ { 2 data }, { 3 data } ],
+   *   [ { 4 data }, { 5 data }, { 6 data } ]
+   * ]
+   *
+   * Note that this function doesn't add labels. This is done after the data
+   * has been generated.
+   */
+  generateAllPoints = () => {
+    const { corners, initialR, meshLevels } = this.props;
+    const rowCount = 2 ** (meshLevels - 1);
+    let pointsWithoutLabels = Array.from({ length: rowCount }, (_, rowIdx) => {
+      if (rowIdx === 0) {
+        return [{ ...corners[0], color: COLORS.BLACK, r: initialR }];
+      }
+      let fraction = rowIdx / rowCount;
+      let [top, left, right] = corners;
+      let firstPoint = this.generatePoint(top, left, fraction);
+      let lastPoint = this.generatePoint(top, right, fraction);
+      if (rowIdx === 1) {
+        return [firstPoint, lastPoint];
+      }
+      let points = [firstPoint];
+      for (var i = 1; i < rowIdx; i++) {
+        let rowFraction = i / rowIdx;
+        let newPoint = this.generatePoint(firstPoint, lastPoint, rowFraction);
+        points.push(newPoint);
+      }
+      points.push(lastPoint);
+      return points;
+    });
+    return this.generateLabels(pointsWithoutLabels);
+  };
+
+  /**
+   * Given an array of arrays of points objects without labels,
+   * this adds labels. Labels are specified uniquely subject to the initial condition
+   * that the top corner is "A", and the two points in the next row are "B" and "C".
+   *
+   * @param {Array} points - points object data without labels
+   */
+  generateLabels = points => {
+    const { names } = this.props;
+    points[0][0].label = names[0][0];
+    points[1][0].label = names[1][0];
+    points[1][1].label = names[2][0];
+    for (let rowIdx = 2; rowIdx < points.length; rowIdx++) {
+      for (let cellIdx = 1; cellIdx < rowIdx; cellIdx++) {
+        // generate labels for interior points in the row
+        let leftParent = points[rowIdx - 1][cellIdx - 1];
+        let rightParent = points[rowIdx - 1][cellIdx];
+        points[rowIdx][cellIdx].label = this.deduceLabel(
+          leftParent,
+          rightParent
+        );
+      }
+      // generate labels for first and last points in the row
+      points[rowIdx][0].label = this.deduceLabel(
+        points[rowIdx - 1][0],
+        points[rowIdx][1]
+      );
+      points[rowIdx][rowIdx].label = this.deduceLabel(
+        points[rowIdx - 1][rowIdx - 1],
+        points[rowIdx][rowIdx - 1]
+      );
+    }
+    return points;
+  };
+
+  /**
+   * Given two point objects pt1 and pt2, and an interpolation value frac1 between
+   * 0 and 1, this creates a new initial point.
+   *
+   * Used in the constructor.
+   *
+   * @param {Object} pt1 - First point
+   * @param {Object} pt2 - Second point
+   * @param {Number} frac - Number between 0 and 1
+   */
+  generatePoint = (pt1, pt2, frac) => {
+    const { initialR } = this.props;
+    return {
+      x: interpolate(pt1.x, pt2.x, frac),
+      y: interpolate(pt1.y, pt2.y, frac),
+      color: COLORS.BLACK,
+      prices: pt1.prices.map((price, i) =>
+        interpolate(price, pt2.prices[i], frac)
+      ),
+      r: initialR
+    };
+  };
+
+  /**
+   * Determines the label of a point given two labeled neighbors
+   * In our example, this uniquely determines the label of a point, since every trinagle should
+   * have distinct labels.
+   * For example, if neighbor1.label is "A" and neighbor2.label is "B",
+   * the only label remaining is "C".
+   *
+   * @param {Object} neighbor1 - the first neighbor object
+   * @param {Object} neighbor2 - the second neighbor object
+   */
+  deduceLabel = (neighbor1, neighbor2) => {
+    const label1 = neighbor1.label;
+    const label2 = neighbor2.label;
+    const { names } = this.props;
+    return names
+      .map(name => name[0])
+      .find(ltr => ltr !== label1 && ltr !== label2);
+  };
 
   /** Get coordinates of the three corners of the main triangle. */
   get corners() {
@@ -88,8 +183,9 @@ class RentDivision extends Component {
 
   /** Gets the active roommate, based on the active point in state */
   getActiveRoommate = () => {
-    const { activePtIdx, points } = this.state;
-    const firstLetter = points[activePtIdx].label;
+    const { activePtLoc, points } = this.state;
+    let [x, y] = activePtLoc;
+    const firstLetter = points[x][y].label;
     const { names } = this.props;
     return names.find(name => name[0] === firstLetter);
   };
@@ -111,19 +207,22 @@ class RentDivision extends Component {
    * can terminate.
    */
   handleRoomChoice = () => {
-    const { activePtIdx, currentColorIdx, points } = this.state;
-    const { roomColors } = this.props;
-    const colorStr = roomColors[currentColorIdx].toUpperCase();
-    const color = COLORS[colorStr];
-    let pointsCopy = [...points];
-    pointsCopy[activePtIdx] = { ...points[activePtIdx], color, r: 20 };
-    this.setState({ currentColorIdx: null, points: pointsCopy }, () => {
-      if (activePtIdx < points.length - 1) {
-        this.advanceToNext();
-      } else {
-        this.refineMesh();
-      }
-    });
+    const { roomColors, initialR } = this.props;
+    this.setState(prevState => {
+      const { activePtLoc, currentColorIdx, points } = prevState;
+      const [x, y] = activePtLoc;
+      const colorStr = roomColors[currentColorIdx].toUpperCase();
+      const color = COLORS[colorStr];
+      const newPtData = { ...points[x][y], color, r: 2 * initialR };
+      const pointsCopy = [...points];
+      const pointsRowCopy = [...pointsCopy[x]];
+      pointsCopy[x] = pointsRowCopy;
+      pointsCopy[x][y] = newPtData;
+      return {
+        currentColorIdx: null,
+        points: pointsCopy
+      };
+    }, this.advanceToNext);
   };
 
   /**
@@ -132,50 +231,36 @@ class RentDivision extends Component {
    * @param {Array} points - Array of updated point objects
    */
   advanceToNext = () => {
+    // TODO - this isn't the actual traversal order we want
     this.setState(prevState => {
-      let { points, activePtIdx } = prevState;
-      while (points[activePtIdx].color !== COLORS.BLACK) {
-        activePtIdx++;
-      }
-      return { activePtIdx };
+      let { activePtLoc } = prevState;
+      let [x, y] = activePtLoc;
+      let newLoc = [x, y + 1];
+      if (y === x) newLoc = [x + 1, 0];
+      return { activePtLoc: newLoc };
     });
   };
 
   /**
-   * Doubles the number of points in the mesh,
-   * by snagging the midpoint between each existing point
-   * in the mesh.
+   * Converts the nested array of point data into an array of LabeledCircle components.
+   *
    */
-  refineMesh = () => {
-    const { points, triangles } = this.state;
-    let { names } = this.props;
-    const pointsCopy = [...points];
-    const newTriangles = [];
-    triangles.forEach(corners => {
-      let newPts = [];
-      corners.forEach((currentPt, idx) => {
-        let nextPt = corners[(idx + 1) % corners.length];
-        let x = (currentPt.x + nextPt.x) / 2;
-        let y = (currentPt.y + nextPt.y) / 2;
-        let prices = currentPt.prices.map(
-          (price, i) => price / 2 + nextPt.prices[i] / 2
-        );
-        let color = COLORS.BLACK;
-        // the roommate who owns a given point
-        // should be the roommate who doesn't own one of the outer points
-        let label = names
-          .map(name => name[0])
-          .find(ltr => ltr !== currentPt.label && ltr !== nextPt.label);
-        newPts.push({ x, y, prices, color, label });
-      });
-      newTriangles.push(
-        [corners[0], newPts[0], newPts[2]],
-        [newPts[0], corners[1], newPts[1]],
-        [newPts[2], newPts[1], corners[2]]
-      );
-      pointsCopy.push(...newPts);
-    });
-    this.setState({ points: pointsCopy, triangles: newTriangles, activePtIdx: 0 }, this.advanceToNext);
+  generateLabeledCircles = () => {
+    const { points, activePtLoc } = this.state;
+    const [activeX, activeY] = activePtLoc;
+    return points.reduce((components, pointRow, x) => {
+      let componentRow = pointRow.map((p, y) => (
+        <LabeledCircle
+          {...p}
+          handleLeave={this.handleTooltipHide}
+          handleUpdate={this.handleTooltipShow.bind(this, p)}
+          key={`${p.x}|${p.y}`}
+          label={p.label}
+          isActive={x === activeX && y === activeY}
+        />
+      ));
+      return [...components, componentRow];
+    }, []);
   };
 
   /**
@@ -194,17 +279,17 @@ class RentDivision extends Component {
   render() {
     const { width, height, roomColors } = this.props;
     const {
-      activePtIdx,
+      activePtLoc,
       currentColorIdx,
       points,
       tooltipVisible,
       tooltipX,
       tooltipY,
-      tooltipBody,
-      triangles
+      tooltipBody
     } = this.state;
     const currentRoommate = this.getActiveRoommate();
-    const activePoint = points[activePtIdx];
+    const [activeX, activeY] = activePtLoc;
+    const activePoint = points[activeX][activeY];
     const { prices } = activePoint;
     const radioButtonLabels = this.getTooltipBody(activePoint).map(
       (text, idx) => ({
@@ -213,20 +298,10 @@ class RentDivision extends Component {
         disabled: this.shouldBeDisabled(prices, idx)
       })
     );
-    const labeledCircles = points.map((p, i) => (
-      <LabeledCircle
-        {...p}
-        handleLeave={this.handleTooltipHide}
-        handleUpdate={this.handleTooltipShow.bind(this, p)}
-        key={`${p.x}|${p.y}`}
-        label={p.label}
-        isActive={i === activePtIdx}
-      />
-    ));
-    let triangleStrokes = triangles.map(corners => {
-      let key = corners.map(c => `${c.x}|${c.y}`).join("|");
-      return <Polygon points={corners} key={key} fill="none" />;
-    });
+    // let triangleStrokes = triangles.map(corners => {
+    //   let key = corners.map(c => `${c.x}|${c.y}`).join("|");
+    //   return <Polygon points={corners} key={key} fill="none" />;
+    // });
     let buttonText = "";
     if (currentColorIdx !== null) {
       let currentColor = roomColors[currentColorIdx].toLowerCase();
@@ -236,8 +311,8 @@ class RentDivision extends Component {
       <ColumnLayout break="extraSmall">
         <div>
           <ClippedSVG width={width} height={height} id="rent">
-            {triangleStrokes}
-            {labeledCircles}
+            {/* {triangleStrokes} */}
+            {this.generateLabeledCircles()}
           </ClippedSVG>
           <Tooltip
             visible={tooltipVisible}
@@ -262,19 +337,52 @@ class RentDivision extends Component {
 }
 
 RentDivision.propTypes = {
+  corners: PropTypes.arrayOf(
+    PropTypes.shape({
+      x: PropTypes.number.isRequired,
+      y: PropTypes.number.isRequired
+    })
+  ).isRequired,
+  height: PropTypes.number.isRequired,
+  initialR: PropTypes.number.isRequired,
+  names: PropTypes.arrayOf(PropTypes.string).isRequired,
   rent: PropTypes.number.isRequired,
   roomColors: PropTypes.arrayOf(PropTypes.string).isRequired,
-  names: PropTypes.arrayOf(PropTypes.string).isRequired,
-  width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired
+  width: PropTypes.number.isRequired
 };
 
+const width = 600;
+const height = 600;
+const rent = 1600;
+const traingleRad = height / 2 - 20;
+const xBase = width / 2;
+const yBase = height / 2 + 70;
+
 RentDivision.defaultProps = {
-  rent: 2400,
-  roomColors: ["Orange", "Green", "Purple"],
+  corners: [
+    {
+      x: xBase + traingleRad * Math.cos(Math.PI / 2),
+      y: yBase - traingleRad * Math.sin(Math.PI / 2),
+      prices: [0, 0, rent]
+    },
+    {
+      x: xBase + traingleRad * Math.cos(Math.PI / 2 + (2 * Math.PI) / 3),
+      y: yBase - traingleRad * Math.sin(Math.PI / 2 + (2 * Math.PI) / 3),
+      prices: [rent, 0, 0]
+    },
+    {
+      x: xBase + traingleRad * Math.cos(Math.PI / 2 + (4 * Math.PI) / 3),
+      y: yBase - traingleRad * Math.sin(Math.PI / 2 + (4 * Math.PI) / 3),
+      prices: [0, rent, 0]
+    }
+  ],
+  height,
+  initialR: 10,
+  meshLevels: 5,
   names: ["Alex", "Brett", "Cameron"],
-  width: 600,
-  height: 600
+  rent,
+  roomColors: ["Orange", "Green", "Purple"],
+  width
 };
 
 export default RentDivision;
