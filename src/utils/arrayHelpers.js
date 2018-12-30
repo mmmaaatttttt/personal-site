321a3a728687565dd30fbc9dbda92277b2ed7d07
Matt Lane <lane.matthew@gmail.com>
2018-12-29 16:32:44 -0800

    adding a frequency counter helper

---
+++ src/utils/arrayHelpers.js
@@ -0,0 +1,18 @@
+/**
+ * Converts an array to a Map of key-value pairs,
+ * where each key is an array element, and each value
+ * is the frequency of the element in the array.
+ *
+ * @param {Array} array of values
+ */
+function generateFreqMap(arr) {
+  const map = new Map;
+  for (let val of arr) {
+    let count = 0;
+    if (map.has(val)) count = map.get(val);
+    map.set(val, count + 1);
+  }
+  return map;
+}
+
+export { generateFreqMap };
