a3852b9c538177fc5d7c1794d89bbecb07f9946a
Matt Lane <lane.matthew@gmail.com>
2019-01-05 07:54:08 -0800

    adding bold option to ColoredSpan

--- src/story_components/atoms/ColoredSpan/index.js
+++ src/story_components/atoms/ColoredSpan/index.js
@@ -3,13 +3,16 @@ import PropTypes from "prop-types";
 
 const ColoredSpan = styled.span`
   color: ${props => props.color};
+  font-weight: ${props => props.bold ? "bold" : "normal"};
 `;
 
 ColoredSpan.propTypes = {
+  bold: PropTypes.bool.isRequired,
   color: PropTypes.string.isRequired
 };
 
 ColoredSpan.defaultProps = {
+  bold: false,
   color: "black"
 };
 
