72a8a5bb2eabe86904e6f635e310d88472bd6768
Matt Lane <lane.matthew@gmail.com>
2019-01-26 14:40:38 -0800

    removing imports from withCaption

--- src/hocs/withCaption.js
+++ src/hocs/withCaption.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import media from "utils/media";
-import PropTypes from "prop-types";
-import { rhythm } from "utils/typography";
 
 const StyledVisualizationContainer = styled.div`
   width: 150%;
