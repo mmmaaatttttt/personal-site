452799d2b75ed8fdf4494eed8f5dd63b58560c54
Matt Lane <lane.matthew@gmail.com>
2019-02-09 08:16:36 -0800

    modifying HTML component so build doesn't break

--- src/html.js
+++ src/html.js
@@ -1,26 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import favicon from "./favicon.ico";
 
-let stylesStr;
-if (process.env.NODE_ENV === `production`) {
-  try {
-    stylesStr = require(`!raw-loader!../public/styles.css`);
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-export default class HTML extends React.Component {
+class HTML extends Component {
   render() {
-    let css;
-    if (process.env.NODE_ENV === `production`) {
-      css = (
-        <style
-          id="gatsby-inlined-css"
-          dangerouslySetInnerHTML={{ __html: stylesStr }}
-        />
-      );
-    }
     return (
       <html {...this.props.htmlAttributes} lang="en">
         <head>
@@ -32,12 +14,12 @@ export default class HTML extends React.Component {
           />
           <link rel="shortcut icon" href={favicon} />
           {this.props.headComponents}
-          {css}
+          {/* {css} */}
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
           <div
-            key={`body`}
+            key="body"
             id="___gatsby"
             dangerouslySetInnerHTML={{ __html: this.props.body }}
           />
@@ -47,3 +29,5 @@ export default class HTML extends React.Component {
     );
   }
 };
+
+export default HTML;
