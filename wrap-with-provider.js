121bd6adf3d1d0e91d63bb181bf66091fc5f41d0
Matt Lane <lane.matthew@gmail.com>
2019-03-09 18:27:56 -0800

    removing commented out redux code

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -3,9 +3,6 @@ import { Provider } from "react-redux";
 
 import createStore from "./src/store/reducers";
 
-// const store = createStore();
-// console.log("STORE", store.getState());
-
 // eslint-disable-next-line react/display-name,react/prop-types
 export default ({ element }) => {
   const store = createStore();
