2f8b0883d3cefb39e43ab267233048d5192762dd
Matt Lane <lane.matthew@gmail.com>
2019-03-18 21:04:31 -0700

    adding more map visualizations

--- src/story_components/templates/VotingMap/index.js
+++ src/story_components/templates/VotingMap/index.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
-import { extent } from "d3-array";
-import { nest } from "d3-collection";
-import { scaleLinear } from "d3-scale";
 import { lighten } from "polished";
 import { format } from "d3-format";
 import withCaption from "hocs/withCaption";
@@ -100,34 +97,33 @@ PureVotingMap.defaultProps = {
     {
       value: "registrations",
       label: "Active Registered Voters",
-      accessor: properties => properties.values[0].active_registration || null,
+      accessor: ({ values }) => values[0].active_registration || null,
       format: ",.0f",
       colors: [COLORS.WHITE, COLORS.ORANGE]
     },
     {
       value: "participants",
       label: "Election Participants",
-      accessor: properties =>
-        properties.values[0].election_participants || null,
+      accessor: ({ values }) => values[0].election_participants || null,
       format: ",.0f",
       colors: [COLORS.WHITE, COLORS.GREEN]
     },
     {
       value: "eligible",
       label: "Eligble Voters (Estimated)",
-      accessor: properties => properties.values[0].eligible_voters_estimated || null,
+      accessor: ({ values }) => values[0].eligible_voters_estimated || null,
       format: ",.0f",
       colors: [COLORS.WHITE, COLORS.DARK_GRAY]
     },
     {
       value: "participantsPerRegistration",
       label: "Participants per 100 Registered Voters",
-      accessor: properties => {
+      accessor: ({ values }) => {
         const {
           election_participants: ep,
           active_registration: ar
-        } = properties.values[0];
-        return ep && ar ? ep / ar * 100 : null;
+        } = values[0];
+        return ep && ar ? (ep / ar) * 100 : null;
       },
       format: ".2f",
       colors: [COLORS.WHITE, COLORS.PURPLE]
@@ -135,12 +131,12 @@ PureVotingMap.defaultProps = {
     {
       value: "registrationsPerEligible",
       label: "Registered Voters per 100 Eligible Voters",
-      accessor: properties => {
+      accessor: ({ values }) => {
         const {
           active_registration: ar,
           eligible_voters_estimated: ev
-        } = properties.values[0];
-        return ar && ev ? ar / ev * 100 : null;
+        } = values[0];
+        return ar && ev ? (ar / ev) * 100 : null;
       },
       format: ".2f",
       colors: [COLORS.WHITE, COLORS.DARK_GREEN]
@@ -148,15 +144,83 @@ PureVotingMap.defaultProps = {
     {
       value: "turnout",
       label: "Participants per 100 Eligible Voters",
-      accessor: properties => {
+      accessor: ({ values }) => {
         const {
           election_participants: ep,
           eligible_voters_estimated: ev
-        } = properties.values[0];
-        return ep && ev ? ep / ev * 100 : null;
+        } = values[0];
+        return ep && ev ? (ep / ev) * 100 : null;
       },
       format: ".2f",
       colors: [COLORS.WHITE, COLORS.DARK_BLUE]
+    },
+    {
+      value: "pollWorkers",
+      label: "Poll Workers",
+      accessor: ({ values }) => values[0].poll_workers || null,
+      format: ",.0f",
+      colors: [COLORS.WHITE, COLORS.ORANGE]
+    },
+    {
+      value: "pollingPlaces",
+      label: "Polling Places",
+      accessor: ({ values }) => values[0].polling_places || null,
+      format: ",.0f",
+      colors: [COLORS.WHITE, COLORS.RED]
+    },
+    {
+      value: "workersPerPlace",
+      label: "Poll Workers per Polling Place",
+      accessor: ({ values }) => {
+        const { poll_workers: pw, polling_places: pp } = values[0];
+        return pw && pp ? pw / pp : null;
+      },
+      format: ".2f",
+      colors: [COLORS.WHITE, COLORS.GREEN]
+    },
+    {
+      value: "workersPerParticipant",
+      label: "Poll Workers per 1,000 Election Participants",
+      accessor: ({ values }) => {
+        const {
+          poll_workers: pw,
+          participants_in_jurisdictions_with_poll_worker_info: ep
+        } = values[0];
+        return pw && ep ? (pw / ep) * 1000 : null;
+      },
+      format: ".2f",
+      colors: [COLORS.WHITE, COLORS.DARK_BLUE]
+    },
+    {
+      value: "placesPerParticipant",
+      label: "Polling places per 1,000 Election Participants",
+      accessor: ({ values }) => {
+        const {
+          polling_places: pp,
+          participants_in_jurisdictions_with_polling_place_info: ep
+        } = values[0];
+        return pp && ep ? (pp / ep) * 1000 : null;
+      },
+      format: ".2f",
+      colors: [COLORS.WHITE, COLORS.DARK_GREEN]
+    },
+    {
+      value: "difficulty",
+      label: "Average Difficulty of Finding Poll Workers",
+      accessor: ({ values }) => {
+        const {
+          difficulty_very_difficult: d5,
+          difficulty_somewhat_difficult: d4,
+          difficulty_neither_difficult_nor_easy: d3,
+          difficulty_somewhat_easy: d2,
+          difficulty_very_easy: d1
+        } = values[0];
+        let numCounts = d1 + d2 + d3 + d4 + d5;
+        let totalDifficulty = 1 * d1 + 2 * d2 + 3 * d3 + 4 * d4 + 5 * d5;
+        return numCounts > 0 ? totalDifficulty / numCounts : null;
+      },
+      format: ".2f",
+      colors: [COLORS.WHITE, COLORS.PURPLE]
     }
   ]
 };
@@ -172,17 +236,14 @@ const query = graphql`
           active_registration
           election_participants
           eligible_voters_estimated
-          jurisdictions_with_precinct_info
           jurisdictions_with_polling_place_info
           jurisdictions_with_poll_worker_count
           jurisdictions_with_age_info
           jurisdictions_with_difficulty_info
-          registrants_in_jurisdictions_with_precinct_info
           registrants_in_jurisdictions_with_polling_place_info
           registrants_in_jurisdictions_with_poll_worker_info
           registrants_in_jurisdictions_with_poll_worker_age_info
           registrants_in_jurisdictions_with_difficulty_info
-          participants_in_jurisdictions_with_precinct_info
           participants_in_jurisdictions_with_polling_place_info
           participants_in_jurisdictions_with_poll_worker_info
           participants_in_jurisdictions_with_poll_worker_age_info
