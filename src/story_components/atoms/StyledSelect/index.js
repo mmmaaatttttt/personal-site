29721e44a99037b3968ac6a561493b0785f7c49b
Matt Lane <lane.matthew@gmail.com>
2019-04-10 21:36:49 -0700

    adding margin to StyledSelect

--- src/story_components/atoms/StyledSelect/index.js
+++ src/story_components/atoms/StyledSelect/index.js
@@ -1,10 +1,16 @@
 import Select from "react-select";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import COLORS from "utils/styles";
 
 const StyledSelect = styled(Select)`
   flex: 1;
+  margin: ${props => props.margin}  
 `;
+
+StyledSelect.propTypes = {
+  margin: PropTypes.string.isRequired
+}
 
 StyledSelect.defaultProps = {
   styles: {
@@ -15,7 +21,8 @@ StyledSelect.defaultProps = {
       ...provided,
       flexWrap: "nowrap"
     })
-  }
+  },
+  margin: "0.75rem 0 0 0"
 };
 
 export default StyledSelect;
