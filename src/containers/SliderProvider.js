578cb8d23c43a3643def176f9bad9e5c28d15b84
Matt Lane <lane.matthew@gmail.com>
2019-04-14 09:40:20 -0700

    updating SliderProvider

--- src/containers/SliderProvider.js
+++ src/containers/SliderProvider.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { SliderGroup } from "story_components";
+import { ColumnLayout, NarrowContainer, SliderGroup } from "story_components";
 
 class SliderProvider extends Component {
-  state = {
-    sliderValues: this.props.sliderData.map(d => d.value)
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      sliderValues: props.initialData.map(d => d.initialValue)
+    };
+    this.handleValueFns = props.initialData.map((_, i) =>
+      this.handleValueChange.bind(this, i)
+    );
+  }
 
   handleValueChange = (idx, newVal) => {
     this.setState(st => {
@@ -15,38 +21,59 @@ class SliderProvider extends Component {
     });
   };
 
-  componentDidMount() {
-    this.handleValueFns = this.props.sliderData.map((_, i) =>
-      this.handleValueChange.bind(this, i)
-    );
-  }
-
   render() {
-    const { sliderData, column } = this.props;
-    const data = sliderData.map((d, i) => ({
+    const { initialData, column, render, width } = this.props;
+    const { sliderValues } = this.state;
+    const dataWithHandlers = initialData.map((d, i) => ({
       ...d,
+      value: sliderValues[i],
       handleValueChange: this.handleValueFns[i]
     }));
-    return (
+    const numSliders = sliderValues.length;
+    const mainContent = (
       <React.Fragment>
-        <SliderGroup data={data} column={column} />
-        {this.props.render(this.state.sliderValues)}
+        <SliderGroup data={dataWithHandlers} column={column} />
+        {render(this.state.sliderValues)}
       </React.Fragment>
+    );
+    return numSliders < 4 ? (
+      <NarrowContainer width={width} fullWidthAt="small">
+        {mainContent}
+      </NarrowContainer>
+    ) : (
+      <ColumnLayout break="small">{mainContent}</ColumnLayout>
     );
   }
 }
 
 SliderProvider.propTypes = {
+  column: PropTypes.bool.isRequired,
+  initialData: PropTypes.arrayOf(
+    PropTypes.shape({
+      min: PropTypes.number.isRequired,
+      max: PropTypes.number.isRequired,
+      initialValue: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      color: PropTypes.string,
+      key: PropTypes.any,
+      tickCount: PropTypes.number,
+      minIcon: PropTypes.string,
+      maxIcon: PropTypes.string,
+      fadeIcons: PropTypes.bool
+    }).isRequired
+  ),
   render: PropTypes.func.isRequired,
-  column: PropTypes.bool.isRequired
+  width: PropTypes.string.isRequired
 };
 
 SliderProvider.defaultProps = {
+  column: true,
+  initialData: [],
   render: function(...args) {
     console.log("Please implement a render!");
     console.log("Here are the render args: ", args);
   },
-  column: true
+  width: "70%"
 };
 
 export default SliderProvider;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,6 @@
 // @create-index
 
+export { default as SliderProvider } from './SliderProvider.js';
 export { default as TooltipProvider } from './TooltipProvider.js';
 export { default as withCaption } from './withCaption.js';
 
