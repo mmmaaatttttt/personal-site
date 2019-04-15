a4eeb868e61a0f37a95164a33365f57cf1aee95b
Matt Lane <lane.matthew@gmail.com>
2019-04-14 20:37:30 -0700

    using SliderProvider in Gerrymandering post

--- src/story_components/templates/GerrymanderHistoricalMap/index.js
+++ src/story_components/templates/GerrymanderHistoricalMap/index.js
@@ -5,16 +5,15 @@ import { extent } from "d3-array";
 import { nest } from "d3-collection";
 import { scaleLinear } from "d3-scale";
 import { lighten } from "polished";
-import { withCaption } from "containers";
+import { withCaption, SliderProvider } from "containers";
 import COLORS from "utils/styles";
 import { calculateWastedVotes } from "utils/mathHelpers";
-import { BarGraph, ColumnLayout, SliderGroup, USMap } from "story_components";
+import { sliderDataType } from "utils/types";
+import { BarGraph, ColumnLayout, USMap } from "story_components";
 
 class PureHistoricalMap extends Component {
   state = {
-    allBarData: [],
-    currentYear: this.props.maxYear,
-    currentMinElectors: 2
+    allBarData: []
   };
 
   addGeometryProperties = (us, data) => {
@@ -62,20 +61,18 @@ class PureHistoricalMap extends Component {
     );
   };
 
-  fillAccessor = d => {
-    let { currentYear, currentMinElectors } = this.state;
-    let relevantData = d.values.filter(val => val.year === currentYear);
-    if (relevantData.length < currentMinElectors) return null;
-    return d.efficiencyGaps[currentYear];
+  fillAccessor = (year, electors) => d => {
+    let relevantData = d.values.filter(val => val.year === year);
+    if (relevantData.length < electors) return null;
+    return d.efficiencyGaps[year];
   };
 
-  getTooltipBody = d => {
-    let { currentYear, currentMinElectors } = this.state;
+  getTooltipBody = (year, electors) => d => {
     let districtsForYear = d.values
-      ? d.values.filter(val => val.year === currentYear).length
+      ? d.values.filter(val => val.year === year).length
       : 1;
-    if (districtsForYear < currentMinElectors) return "Not enough districts.";
-    let gap = d.efficiencyGaps[currentYear];
+    if (districtsForYear < electors) return "Not enough districts.";
+    let gap = d.efficiencyGaps[year];
     let favoredParty = gap < 0 ? "Democrats" : "Republicans";
     let formattedGap = Math.abs(gap * 100);
     return [
@@ -85,36 +82,9 @@ class PureHistoricalMap extends Component {
     ];
   };
 
-  handleSliderUpdate = (key, val) => {
-    this.setState({ [key]: val });
-  };
-
   render() {
-    const { allBarData, currentYear, currentMinElectors } = this.state;
-    const { minElectors, maxElectors, minYear, maxYear, data } = this.props;
-    const sliderData = [
-      {
-        title: `Year: ${currentYear}`,
-        min: minYear,
-        max: maxYear,
-        value: currentYear,
-        step: 2,
-        handleValueChange: this.handleSliderUpdate.bind(this, "currentYear"),
-        color: COLORS.DARK_GRAY
-      },
-      {
-        title: `Minimum Number of Electors: ${currentMinElectors}`,
-        min: minElectors,
-        max: maxElectors,
-        value: currentMinElectors,
-        step: 1,
-        handleValueChange: this.handleSliderUpdate.bind(
-          this,
-          "currentMinElectors"
-        ),
-        color: COLORS.DARK_GRAY
-      }
-    ];
+    const { allBarData } = this.state;
+    const { data, sliderData } = this.props;
     // pararemters for bar graph
     let width = 1600;
     let height = 900;
@@ -123,54 +93,69 @@ class PureHistoricalMap extends Component {
     let yScale = scaleLinear()
       .domain([0, 6])
       .range([height - padding, padding]);
-    let currentBarData = allBarData
-      .filter(
-        // throw out bars coming from states with
-        // a small number of electors for the current year
-        barD =>
-          data.filter(d => d.year === currentYear && d.state === barD.name)
-            .length >= currentMinElectors
-      )
-      .map(d => {
-        let seatGap = d.seatGaps[currentYear];
-        let height = Math.abs(seatGap);
-        let color = COLORS.DARK_BLUE;
-        if (seatGap > 0) color = COLORS.RED;
-        if (height < 2) color = lighten(0.4, color);
-        return {
-          key: d.label,
-          height,
-          color
-        };
-      })
-      .sort((a, b) => a.height - b.height);
     return (
       <div>
-        <SliderGroup data={sliderData} />
-        <ColumnLayout break="small">
-          <USMap
-            addGeometryProperties={this.addGeometryProperties}
-            colors={[COLORS.DARK_BLUE, COLORS.WHITE, COLORS.RED]}
-            data={data}
-            domain={[-egMax, 0, egMax]}
-            fillAccessor={this.fillAccessor}
-            getTooltipTitle={d => d.name}
-            getTooltipBody={this.getTooltipBody}
-          />
-          <div>
-            <BarGraph
-              barData={currentBarData}
-              barLabel={bar => bar.key}
-              height={height}
-              labelFontSize={`${(currentMinElectors - 1) / 10 + 1.2}rem`}
-              padding={padding}
-              svgId="eg-chart"
-              tickStep={2}
-              width={width}
-              yScale={yScale}
-            />
-          </div>
-        </ColumnLayout>
+        <SliderProvider
+          initialData={sliderData}
+          width="100%"
+          render={sliderVals => {
+            const [currentYear, currentMinElectors] = sliderVals;
+            let currentBarData = allBarData
+              .filter(
+                // throw out bars coming from states with
+                // a small number of electors for the current year
+                barD =>
+                  data.filter(
+                    d => d.year === currentYear && d.state === barD.name
+                  ).length >= currentMinElectors
+              )
+              .map(d => {
+                let seatGap = d.seatGaps[currentYear];
+                let height = Math.abs(seatGap);
+                let color = COLORS.DARK_BLUE;
+                if (seatGap > 0) color = COLORS.RED;
+                if (height < 2) color = lighten(0.4, color);
+                return {
+                  key: d.label,
+                  height,
+                  color
+                };
+              })
+              .sort((a, b) => a.height - b.height);
+            return (
+              <ColumnLayout break="small">
+                <USMap
+                  addGeometryProperties={this.addGeometryProperties}
+                  colors={[COLORS.DARK_BLUE, COLORS.WHITE, COLORS.RED]}
+                  data={data}
+                  domain={[-egMax, 0, egMax]}
+                  fillAccessor={this.fillAccessor(
+                    currentYear,
+                    currentMinElectors
+                  )}
+                  getTooltipTitle={d => d.name}
+                  getTooltipBody={this.getTooltipBody(
+                    currentYear,
+                    currentMinElectors
+                  )}
+                />
+                <div>
+                  <BarGraph
+                    barData={currentBarData}
+                    barLabel={bar => bar.key}
+                    height={height}
+                    labelFontSize={`${(currentMinElectors - 1) / 10 + 1.2}rem`}
+                    padding={padding}
+                    svgId="eg-chart"
+                    tickStep={2}
+                    width={width}
+                    yScale={yScale}
+                  />
+                </div>
+              </ColumnLayout>
+            );
+          }}
+        />
       </div>
     );
   }
@@ -230,15 +215,39 @@ PureHistoricalMap.propTypes = {
   maxElectors: PropTypes.number.isRequired,
   maxYear: PropTypes.number.isRequired,
   minElectors: PropTypes.number.isRequired,
-  minYear: PropTypes.number.isRequired
+  minYear: PropTypes.number.isRequired,
+  sliderData: sliderDataType
 };
+
+// const MIN_YEAR = 1996;
+// const MAX_YEAR = 2016;
+// const MIN_ELECTORS = 2;
+// const MAX_ELECTORS = 10;
 
 PureHistoricalMap.defaultProps = {
   data: [],
-  maxElectors: 10,
-  maxYear: 2016,
-  minElectors: 2,
-  minYear: 1996
+  // maxElectors: MAX_ELECTORS,
+  // maxYear: MAX_YEAR,
+  // minElectors: MIN_ELECTORS,
+  // minYear: MIN_YEAR,
+  sliderData: [
+    {
+      title: val => `Year: ${val}`,
+      min: 1996,
+      max: 2016,
+      initialValue: 2016,
+      step: 2,
+      color: COLORS.DARK_GRAY
+    },
+    {
+      title: val => `Minimum Number of Electors: ${val}`,
+      min: 2,
+      max: 10,
+      initialValue: 2,
+      step: 1,
+      color: COLORS.DARK_GRAY
+    }
+  ]
 };
 
 const query = graphql`
