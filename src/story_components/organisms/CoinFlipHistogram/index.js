19b52e1f9ca65a5c7082d3860e10393265cc5e24
Matt Lane <lane.matthew@gmail.com>
2019-04-14 19:45:58 -0700

    refactoring CoinFlipHistogram to use SliderProvider

--- src/story_components/organisms/CoinFlipHistogram/index.js
+++ src/story_components/organisms/CoinFlipHistogram/index.js
@@ -2,20 +2,11 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { scaleLinear } from "d3-scale";
 import { max } from "d3-array";
-import { withCaption } from "containers";
-import { BarGraph, NarrowContainer, SliderGroup } from "story_components";
+import { withCaption, SliderProvider } from "containers";
+import { BarGraph } from "story_components";
 import COLORS from "utils/styles";
 
 class CoinFlipHistogram extends PureComponent {
-  state = {
-    headsProb: 0.5,
-    numTrials: 20
-  };
-
-  handleSliderChange = (stateKey, val) => {
-    this.setState({ [stateKey]: val });
-  };
-
   binomialDensityValues(n, p) {
     if (p === 0 || p === 1) return Array.from({ length: n + 1 }).fill(p);
     let masses = [(1 - p) ** n];
@@ -31,76 +22,79 @@ class CoinFlipHistogram extends PureComponent {
   }
 
   render() {
-    const { headsProb, numTrials } = this.state;
-    const { height, width, padding } = this.props;
-    
-    let sliderData = [
-      {
-        min: 1,
-        max: 100,
-        step: 1,
-        value: numTrials,
-        title: `Number of coin flips: ${numTrials}`,
-        handleValueChange: val => this.handleSliderChange("numTrials", val),
-        color: COLORS.GREEN,
-        minIcon: "circle",
-        maxIcon: "coins"
-      },
-      {
-        min: 0,
-        max: 1,
-        step: 0.01,
-        value: headsProb,
-        title: `Probability of flipping heads: ${(headsProb * 100).toFixed(0)}%`,
-        handleValueChange: val => this.handleSliderChange("headsProb", val),
-        color: COLORS.GREEN,
-        minIcon: "times-circle",
-        maxIcon: "check-circle"
-      }
-    ];
-    let barData = this.binomialDensityValues(numTrials, headsProb).map(
-      (height, key) => ({ key, height, x0: key, x1: key + 1 })
-    );
-    let maxBarHeight = max(barData, d => d.height);
-    let yScale = scaleLinear()
-                   .domain([0, max([maxBarHeight, 0.1])])
-                   .range([height - padding.bottom, padding.top]);
+    const { height, width, padding, sliderData } = this.props;
     return (
-      <NarrowContainer width="60%" fullWidthAt="small">
-        <SliderGroup data={sliderData} />
-        <BarGraph
-          barData={barData}
-          color={COLORS.GREEN}
-          height={height}
-          histogram
-          padding={padding}
-          svgId="coinflips"
-          thresholds={barData.map(d => d.key).concat(numTrials+1)}
-          yTickFormat={".0%"}
-          tickStep={0.02}
-          width={width}
-          yScale={yScale}
-        />
-      </NarrowContainer>
+      <SliderProvider
+        initialData={sliderData}
+        width="60%"
+        render={sliderVals => {
+          const [ numTrials, headsProb ] = sliderVals;
+          const barData = this.binomialDensityValues(numTrials, headsProb).map(
+            (height, key) => ({ key, height, x0: key, x1: key + 1 })
+          );
+          const maxBarHeight = max(barData, d => d.height);
+          const yScale = scaleLinear()
+            .domain([0, max([maxBarHeight, 0.1])])
+            .range([height - padding.bottom, padding.top]);
+          return (
+            <BarGraph
+              barData={barData}
+              color={COLORS.GREEN}
+              height={height}
+              histogram
+              padding={padding}
+              svgId="coinflips"
+              thresholds={barData.map(d => d.key).concat(numTrials + 1)}
+              yTickFormat={".0%"}
+              tickStep={0.02}
+              width={width}
+              yScale={yScale}
+            />
+          );
+        }}
+      />
     );
   }
 }
 
 CoinFlipHistogram.propTypes = {
   height: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired,
   padding: PropTypes.object.isRequired,
+  sliderData: PropTypes.array.isRequired,
+  width: PropTypes.number.isRequired
 };
 
 CoinFlipHistogram.defaultProps = {
   height: 400,
-  width: 600,
   padding: {
     top: 10,
     left: 10,
     bottom: 20,
     right: 10
-  }
+  },
+  sliderData: [
+    {
+      min: 1,
+      max: 100,
+      step: 1,
+      initialValue: 20,
+      title: val => `Number of coin flips: ${val}`,
+      color: COLORS.GREEN,
+      minIcon: "circle",
+      maxIcon: "coins"
+    },
+    {
+      min: 0,
+      max: 1,
+      step: 0.01,
+      initialValue: 0.05,
+      title: val => `Probability of flipping heads: ${(val * 100).toFixed(0)}%`,
+      color: COLORS.GREEN,
+      minIcon: "times-circle",
+      maxIcon: "check-circle"
+    }
+  ],
+  width: 600
 };
 
 export default withCaption(CoinFlipHistogram);
