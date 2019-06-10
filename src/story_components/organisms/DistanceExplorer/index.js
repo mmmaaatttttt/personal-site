4709117ad5a13c323819de0279622bf55935cdd9
Matt Lane <lane.matthew@gmail.com>
2019-06-09 20:45:03 -0700

    using hooks for DistanceExplorer

--- src/story_components/organisms/DistanceExplorer/index.js
+++ src/story_components/organisms/DistanceExplorer/index.js
@@ -1,51 +1,36 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useCallback } from "react";
 import { DraggableCircle, Graph, NarrowContainer } from "story_components";
 import { svgProps, svgDefaultProps } from "utils/types";
 
-class DistanceExplorer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      points: [{ x: 2, y: 2 }, { x: -2, y: -2 }]
-    };
-  }
-
-  handleDrag = (idx, x, y) => {
-    this.setState(st => {
-      const pointsCopy = [...st.points];
-      const { xScale, yScale } = this.props;
-      pointsCopy[idx] = { x: xScale.invert(x), y: yScale.invert(y) };
-      return { points: pointsCopy };
-    });
-  };
-
-  render() {
-    const { points } = this.state;
-    const { width, height, xScale, yScale } = this.props;
-    return (
-      <NarrowContainer>
-        <Graph
-          xAxisPosition="center"
-          yAxisPosition="center"
-          width={width}
-          height={height}
-          xScale={xScale}
-          yScale={yScale}
-        >
-          {points.map((point, idx) => (
-            <DraggableCircle
-              key={idx}
-              cx={xScale(point.x)}
-              cy={yScale(point.y)}
-              onDrag={({ x, y }) => this.handleDrag(idx, x, y)}
-            />
-          ))}
-        </Graph>
-      </NarrowContainer>
-    );
-    // return <Graph />;
-  }
+function DistanceExplorer({ width, height, xScale, yScale }) {
+  const [points, setPoints] = useState([{x: 2, y: 2}, {x: -2, y: -2}])
+  const handleDrag = useCallback((idx, {x, y}) => {
+    const pointsCopy = [...points];
+    pointsCopy[idx] = { x: xScale.invert(x), y: yScale.invert(y) };
+    setPoints(pointsCopy);
+  }, [points, setPoints, xScale, yScale])
+  return (
+    <NarrowContainer>
+      <Graph
+        xAxisPosition="center"
+        yAxisPosition="center"
+        width={width}
+        height={height}
+        xScale={xScale}
+        yScale={yScale}
+      >
+        {points.map((point, idx) => (
+          <DraggableCircle
+            key={idx}
+            id={idx}
+            cx={xScale(point.x)}
+            cy={yScale(point.y)}
+            onDrag={handleDrag}
+          />
+        ))}
+      </Graph>
+    </NarrowContainer>
+  );
 }
 
 DistanceExplorer.propTypes = {
@@ -56,4 +41,4 @@ DistanceExplorer.defaultProps = {
   ...svgDefaultProps
 };
 
-export default DistanceExplorer;
+export default React.memo(DistanceExplorer);
