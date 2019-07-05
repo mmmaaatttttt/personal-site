bf7f92c2e2da846a7495c8f8a42fc31e1c135268
Matt Lane <lane.matthew@gmail.com>
2019-07-05 07:33:54 -0700

    resizing ManhattanCircle component

--- src/story_components/templates/ManhattanCircle/index.js
+++ src/story_components/templates/ManhattanCircle/index.js
@@ -1,51 +1,55 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { scaleLinear } from "d3-scale";
-import { Graph, NarrowContainer } from "story_components";
+import { Graph } from "story_components";
 import { SliderProvider, withCaption } from "providers";
 import { generateCirclePoints } from "./helpers";
 import COLORS from "utils/styles";
 
 function ManhattanCircle({ height, width }) {
   return (
-    <NarrowContainer width="50%">
-      <SliderProvider
-        initialData={[
-          {
-            min: 1,
-            max: 100,
-            initialValue: 1,
-            step: 1,
-            title: r => `Circle radius: ${r}`
-          }
-        ]}
-        render={([r]) => {
-          const bound = Math.max(r + 1, 10);
-          const xScale = scaleLinear()
-            .domain([-bound, bound])
-            .range([0, width]);
-          const yScale = scaleLinear()
-            .domain([-bound, bound])
-            .range([height, 0]);
-          const points = generateCirclePoints(r);
-          return (
-            <Graph
-              xAxisPosition="center"
-              yAxisPosition="center"
-              width={width}
-              height={height}
-              xScale={xScale}
-              yScale={yScale}
-            >
-              <circle cx={xScale(0)} cy={yScale(0)} fill={COLORS.RED} r={8} />
-              {points.map(pt => (
-                <circle cx={xScale(pt.x)} cy={yScale(pt.y)} r={4} key={`${pt.x}|${pt.y}`} />
-              ))}
-            </Graph>
-          );
-        }}
-      />
-    </NarrowContainer>
+    <SliderProvider
+      width="50%"
+      initialData={[
+        {
+          min: 1,
+          max: 100,
+          initialValue: 1,
+          step: 1,
+          title: r => `Circle radius: ${r}`
+        }
+      ]}
+      render={([r]) => {
+        const bound = Math.max(r + 1, 10);
+        const xScale = scaleLinear()
+          .domain([-bound, bound])
+          .range([0, width]);
+        const yScale = scaleLinear()
+          .domain([-bound, bound])
+          .range([height, 0]);
+        const points = generateCirclePoints(r);
+        return (
+          <Graph
+            xAxisPosition="center"
+            yAxisPosition="center"
+            width={width}
+            height={height}
+            xScale={xScale}
+            yScale={yScale}
+          >
+            <circle cx={xScale(0)} cy={yScale(0)} fill={COLORS.RED} r={8} />
+            {points.map(pt => (
+              <circle
+                cx={xScale(pt.x)}
+                cy={yScale(pt.y)}
+                r={4}
+                key={`${pt.x}|${pt.y}`}
+              />
+            ))}
+          </Graph>
+        );
+      }}
+    />
   );
 }
 
