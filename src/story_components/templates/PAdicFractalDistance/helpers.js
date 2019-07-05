7bd314beefa24ec3bac7cfbe1c527442a40d26d6
Matt Lane <lane.matthew@gmail.com>
2019-07-05 07:34:26 -0700

    finishing p-adic fractal distance explorer

--- src/story_components/templates/PAdicFractalDistance/helpers.js
+++ src/story_components/templates/PAdicFractalDistance/helpers.js
@@ -4,10 +4,77 @@ function generatePAdicPoints(prime, level) {
     return {
       cx: Math.cos(angle),
       cy: Math.sin(angle),
-      label: i + 1
+      fillIdx: 0,
+      num: i
     };
   });
+  while (points.length < prime ** level) {
+    const nextNum = points.length;
+    const pAdicDigits = _pAdicExpansion(nextNum, prime);
+    const lastDigit = pAdicDigits[pAdicDigits.length - 1];
+    const highestPower = pAdicDigits.length - 1;
+    points.push({
+      cx: _pointSum(points, nextNum, highestPower, lastDigit, prime, "cx"),
+      cy: _pointSum(points, nextNum, highestPower, lastDigit, prime, "cy"),
+      fillIdx: Math.floor(Math.log(nextNum) / Math.log(prime)),
+      num: nextNum
+    });
+  }
   return points;
 }
 
-export { generatePAdicPoints };
+// recurrence relation for generating graphical representation
+// of a point in p-adic space given points already calculated.
+function _pointSum(points, n, k, a_k, prime, key) {
+  const firstIdx = n % prime ** k;
+  const secondIdx = (n + a_k * prime ** (k - 1)) % prime ** k;
+  return ((prime - 1) * points[firstIdx][key] + points[secondIdx][key]) / prime;
+}
+
+// return the p-adic expansion of a number as an array.
+// Example: _pAdicExpansion(11, 3) -> [2, 0, 1]
+function _pAdicExpansion(num, prime) {
+  if (num < 0) return "num must be a nonnegative integer.";
+  let maxPrimePower = _largestPrimePower(num, prime);
+  let coefficients = Array.from({ length: maxPrimePower + 1 }).fill(0);
+  for (let currentExp = coefficients.length - 1; currentExp > 0; currentExp--) {
+    const currentCoeff = Math.floor(num / prime ** currentExp);
+    coefficients[currentExp] = currentCoeff;
+    num -= currentCoeff * prime ** currentExp;
+  }
+  coefficients[0] = num;
+  return coefficients;
+}
+
+// returns the largest power of p which is less than n.
+// Example: _largestPrimePower(11, 3) should return 2
+function _largestPrimePower(n, p) {
+  if (n === 0) return 0;
+  return Math.floor(Math.log(n) / Math.log(p));
+}
+
+function animationStart(num, prime, points, colorsArr) {
+  let firstIdx = 0;
+  let exponent = _largestPrimePower(num, prime);
+  if (exponent > 0) firstIdx = num % prime ** exponent;
+  return { ...points[firstIdx], fill: colorsArr[points[firstIdx].fillIdx], opacity: 0 };
+}
+
+function animationLeave(d, i) {
+  return {
+    opacity: [0],
+    timing: { duration: 500, delay: i * 10 }
+  };
+}
+
+function animationUpdate(d, i, colorsArr) {
+  return {
+    cx: [d.cx],
+    cy: [d.cy],
+    fill: [colorsArr[d.fillIdx]],
+    opacity: [1],
+    timing: { duration: 500, delay: i * 10 }
+  };
+}
+
+export { generatePAdicPoints, animationStart, animationUpdate, animationLeave };
--- src/story_components/templates/PAdicFractalDistance/index.js
+++ src/story_components/templates/PAdicFractalDistance/index.js
@@ -1,32 +1,98 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { scaleLinear } from "d3-scale";
-import { SelectProvider, withCaption } from "providers";
+import NodeGroup from "react-move/NodeGroup";
+import { SelectProvider, SliderProvider, withCaption } from "providers";
 import { CenteredSVGText, ClippedSVG } from "story_components";
-import { generatePAdicPoints } from "./helpers";
+import {
+  generatePAdicPoints,
+  animationStart,
+  animationUpdate,
+  animationLeave
+} from "./helpers";
 import COLORS from "utils/styles";
 
-//TODO
-//2. Slider to update the number of fractal iterations.
+function fontSize(prime, level) {
+  if (level === 3 && prime > 3) return "0%";
+  return "100%";
+}
 
-function PAdicFractalDistance({ primes, xScale, yScale }) {
+function showText(prime, level, num) {
+  return (prime === 3 || level < 3) && num < prime ** level;
+}
+
+function PAdicFractalDistance({ levelColors, primes, xScale, yScale }) {
   return (
-    <SelectProvider
-      options={[primes.map(p => ({ value: p, label: `Selected prime: ${p}` }))]}
-      render={([{ value: prime }]) => {
-        const points = generatePAdicPoints(prime, 1);
+    <SliderProvider
+      initialData={[
+        {
+          color: COLORS.BLACK,
+          min: 1,
+          max: 3,
+          step: 1,
+          initialValue: 1,
+          minIcon: "compress-arrows-alt",
+          maxIcon: "expand-arrows-alt",
+          title: val => `Number of generations: ${val}`,
+          tickCount: 3
+        }
+      ]}
+      width="50%"
+      render={([level]) => {
         return (
-          <ClippedSVG id="p-adic-distances">
-            {points.map(pt => {
-              const x = xScale(pt.cx);
-              const y = yScale(pt.cy);
+          <SelectProvider
+            options={[
+              primes.map(p => ({ value: p, label: `Selected prime: ${p}` }))
+            ]}
+            width="100%"
+            render={([{ value: prime }]) => {
+              const points = generatePAdicPoints(prime, level);
               return (
-              <React.Fragment key={`${x}|${y}`}>
-                <circle cx={x} cy={y} r={5} />
-                <CenteredSVGText x={x} y={y} dy={-15}>{pt.label}</CenteredSVGText>
-              </React.Fragment>
-            )})}
-          </ClippedSVG>
+                <ClippedSVG id="p-adic-distances">
+                  <NodeGroup
+                    data={points}
+                    keyAccessor={(_, i) => i}
+                    start={(_, num) =>
+                      animationStart(num, prime, points, levelColors)
+                    }
+                    enter={(d, i) => animationUpdate(d, i, levelColors)}
+                    update={(d, i) => animationUpdate(d, i, levelColors)}
+                    leave={animationLeave}
+                  >
+                    {nodePts => (
+                      <React.Fragment>
+                        {nodePts.map(
+                          ({
+                            key,
+                            data: { num },
+                            state: { cx, cy, fill, opacity }
+                          }) => {
+                            const x = xScale(cx);
+                            const y = yScale(cy);
+                            return (
+                              <g key={key} fill={fill} style={{ opacity }}>
+                                <circle cx={x} cy={y} r={3} />
+                                {showText(prime, level, num) && (
+                                  <CenteredSVGText
+                                    x={x}
+                                    y={y}
+                                    dy={-15}
+                                    fontSize={fontSize(prime, level)}
+                                  >
+                                    {num}
+                                  </CenteredSVGText>
+                                )}
+                              </g>
+                            );
+                          }
+                        )}
+                      </React.Fragment>
+                    )}
+                  </NodeGroup>
+                </ClippedSVG>
+              );
+            }}
+          />
         );
       }}
     />
@@ -34,6 +100,7 @@ function PAdicFractalDistance({ primes, xScale, yScale }) {
 }
 
 PAdicFractalDistance.propTypes = {
+  levelColors: PropTypes.arrayOf(PropTypes.string).isRequired,
   primes: PropTypes.arrayOf(PropTypes.number).isRequired,
   xScale: PropTypes.func.isRequired,
   yScale: PropTypes.func.isRequired
@@ -44,9 +111,14 @@ const height = 600;
 const padding = 30;
 
 PAdicFractalDistance.defaultProps = {
-  primes: [3, 5, 7, 11],
-  xScale: scaleLinear().domain([-1, 1]).range([padding, width - padding]),
-  yScale: scaleLinear().domain([-1, 1]).range([height - padding, padding])
+  levelColors: [COLORS.BLACK, COLORS.BLUE, COLORS.ORANGE],
+  primes: [3, 5, 7],
+  xScale: scaleLinear()
+    .domain([-1, 1])
+    .range([padding, width - padding]),
+  yScale: scaleLinear()
+    .domain([-1, 1])
+    .range([height - padding, padding])
 };
 
 export default withCaption(React.memo(PAdicFractalDistance));
