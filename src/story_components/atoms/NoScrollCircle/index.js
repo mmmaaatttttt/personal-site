92a913e21046cedf39839311e94a56319e50e16d
Matt Lane <lane.matthew@gmail.com>
2019-07-08 19:43:00 -0700

    Using react-draggable for DraggableCircle
    works for DistanceExplorer
    
    need to check:
    
    - FunctionDistanceExplorer
    - InteractivePolygon

--- src/story_components/atoms/NoScrollCircle/index.js
+++ src/story_components/atoms/NoScrollCircle/index.js
@@ -4,7 +4,8 @@ const NoScrollCircle = styled.circle`
   touch-action: none;
   transition: stroke-width 300ms;
 
-  &:hover {
+  &:hover,
+  &:active {
     fill: ${props => props.stroke};
     stroke-width: ${props => props.r * 1.5};
     cursor: pointer;
--- src/story_components/molecules/DraggableCircle/index.js
+++ src/story_components/molecules/DraggableCircle/index.js
@@ -1,75 +1,59 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import Draggable from "react-draggable";
 import { NoScrollCircle } from "story_components";
 import COLORS from "utils/styles";
+import { SVGContext } from "contexts";
 
-class DraggableCircle extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dragging: false
-    };
-    this.circle = React.createRef()
-  }
-
-  getMousePosition = e => {
-    // clientX and clientY need to be normalized
-    // see http://www.petercollingridge.co.uk/tutorials/svg/interactive/dragging/
-    let CTM = this.circle.current.getScreenCTM();
-    let clientX, clientY;
-    try {
-      // try touch event first
-      clientX = e.touches[0].clientX;
-      clientY = e.touches[0].clientY;
-    } catch (err) {
-      // no go, assume mouse event
-      clientX = e.clientX;
-      clientY = e.clientY;
-    }
-    return {
-      x: (clientX - CTM.e) / CTM.a,
-      y: (clientY - CTM.f) / CTM.d
-    };
+function handleDrag(id, onDrag) {
+  return function(_, { x, y }) {
+    onDrag(id, { x, y });
   };
+}
 
-  handleDragStart = () => {
-    this.setState({ dragging: true }, this.props.onDragStart);
+function getBounds(svgBounds, circleR) {
+  const { width, height, padding } = svgBounds;
+  const circleOffset = circleR * 2;
+  return {
+    top: padding.top + circleOffset,
+    left: padding.left + circleOffset,
+    right: width - padding.right - circleOffset,
+    bottom: height - padding.bottom - circleOffset
   };
+}
 
-  handleDrag = e => {
-    if (this.state.dragging) {
-      let coords = this.getMousePosition(e);
-      const { id, onDrag } = this.props;
-      onDrag(id, coords);
-    }
-  };
-
-  handleDragEnd = () => {
-    this.setState({ dragging: false }, this.props.onDragEnd);
-  };
-
-  render() {
-    const { cx, cy, r, fill, stroke, strokeWidth } = this.props;
-    return (
+function DraggableCircle({
+  cx,
+  cy,
+  r,
+  fill,
+  stroke,
+  strokeWidth,
+  id,
+  onDrag,
+  onDragStart,
+  onDragEnd
+}) {
+  const svgBounds = useContext(SVGContext);
+  return (
+    <Draggable
+      onStart={onDragStart}
+      onDrag={handleDrag(id, onDrag)}
+      onStop={onDragEnd}
+      defaultPosition={{ x: cx, y: cy }}
+      bounds={getBounds(svgBounds, r)}
+    >
       <NoScrollCircle
-        cx={cx}
-        cy={cy}
+        bounds="parent"
+        cx={0}
+        cy={0}
         fill={fill}
         r={r}
         stroke={stroke}
         strokeWidth={strokeWidth}
-        onMouseDown={this.handleDragStart}
-        onTouchStart={this.handleDragStart}
-        onMouseMove={this.handleDrag}
-        onTouchMove={this.handleDrag}
-        onMouseUp={this.handleDragEnd}
-        onMouseLeave={this.handleDragEnd}
-        onTouchCancel={this.handleDragEnd}
-        onTouchEnd={this.handleDragEnd}
-        ref={this.circle}
       />
-    );
-  }
+    </Draggable>
+  );
 }
 
 DraggableCircle.propTypes = {
@@ -77,22 +61,25 @@ DraggableCircle.propTypes = {
   cy: PropTypes.number.isRequired,
   fill: PropTypes.string.isRequired,
   id: PropTypes.any.isRequired,
-  onDragStart: PropTypes.func,
-  onDrag: PropTypes.func,
-  onDragEnd: PropTypes.func,
+  onDragStart: PropTypes.func.isRequired,
+  onDrag: PropTypes.func.isRequired,
+  onDragEnd: PropTypes.func.isRequired,
   r: PropTypes.number.isRequired,
   stroke: PropTypes.string.isRequired,
-  strokeWidth: PropTypes.number.isRequired,
+  strokeWidth: PropTypes.number.isRequired
 };
 
 DraggableCircle.defaultProps = {
   cx: 0,
   cy: 0,
-  id: 0,
-  r: 8,
   fill: COLORS.BLACK,
+  id: 0,
+  onDragStart: () => {},
+  onDrag: () => {},
+  onDragEnd: () => {},
+  r: 8,
   stroke: COLORS.BLACK,
   strokeWidth: 0
 };
 
-export default DraggableCircle;
+export default React.memo(DraggableCircle);
