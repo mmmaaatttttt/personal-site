3f14015e254bd61c90e20bc86591eda3ce7dddf4
Matt Lane <lane.matthew@gmail.com>
2019-07-18 15:21:23 -0700

    adding ability to filter by tag

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,11 +1,12 @@
 import React, { useState, useMemo } from "react";
 import styled from "styled-components";
+import { flatten } from "lodash";
 import { graphql } from "gatsby";
 import MainLayout from "layouts/MainLayout";
 import StoryCard from "layouts/StoryCard";
 import { rhythm } from "utils/typography";
 import media from "utils/media";
-import { StyledSelect } from "story_components";
+import Select from "react-select";
 
 const StyledStoriesWrapper = styled.div`
   margin: ${rhythm(0.5)};
@@ -19,44 +20,91 @@ const StyledStoriesWrapper = styled.div`
   `}
 `;
 
+const StyledSelectArea = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  & > div {
+    margin-bottom: ${rhythm(0.75)};
+    width: 49%;
+  }
+
+  ${media.small`
+    & > div {
+      width: 100%;
+      margin-bottom: ${rhythm(0.25)};
+    }
+  `}
+`;
+
 function _extractYear(nodeObj) {
   return +nodeObj.node.frontmatter.date.slice(-4);
 }
 
-function getSortedYearOptions(nodes) {
+function getYearOptions(nodes) {
   const years = nodes.map(_extractYear);
   const yearOptions = Array.from(new Set(years), year => ({
     value: year,
     label: year
   }));
-  return [{ value: "all", label: "All years" }, ...yearOptions];
+  return yearOptions;
+}
+
+function getTagOptions(nodes) {
+  const tags = flatten(nodes.map(({ node }) => node.frontmatter.tags));
+  const tagOptions = Array.from(new Set(tags))
+    .sort()
+    .map(tag => ({
+      value: tag,
+      label: tag
+    }));
+  return tagOptions;
 }
 
 function filterByYear(nodeObj, yearOption) {
-  if (yearOption.value === "all") return true;
-  if (_extractYear(nodeObj) === yearOption.value) return true;
-  return false;
+  if (!yearOption) return true;
+  return yearOption.value === _extractYear(nodeObj);
+}
+
+function filterByActiveTags({ node }, activeTags) {
+  if (activeTags.length === 0) return true;
+  for (let { value } of activeTags) {
+    if (!node.frontmatter.tags.includes(value)) return false;
+  }
+  return true;
 }
 
 const Stories = ({ data, location }) => {
-  const yearOptions = useMemo(() => getSortedYearOptions(data.allMdx.edges), [
-    data
-  ]);
-  const [yearOption, setYearOption] = useState(yearOptions[0]);
+  const yearOptions = useMemo(() => getYearOptions(data.allMdx.edges), [data]);
+  const tagOptions = useMemo(() => getTagOptions(data.allMdx.edges), [data]);
+  const [yearOption, setYearOption] = useState(null);
+  const [activeTags, setActiveTags] = useState([]);
+  const matchingStories = data.allMdx.edges
+    .filter(nodeObj => filterByYear(nodeObj, yearOption))
+    .filter(nodeObj => filterByActiveTags(nodeObj, activeTags));
+
   return (
     <MainLayout location={location}>
       <StyledStoriesWrapper>
-        <StyledSelect
-          flex="none"
-          margin="0 0 1rem"
-          options={yearOptions}
-          value={yearOption}
-          onChange={newOption => setYearOption(newOption)}
-        />
+        <StyledSelectArea>
+          <Select
+            options={yearOptions}
+            onChange={option => setYearOption(option)}
+            isClearable
+            placeholder="Filter by year..."
+          />
+          <Select
+            options={tagOptions}
+            onChange={tagsArr => setActiveTags(tagsArr)}
+            isClearable
+            isMulti
+            placeholder="Filter by tag..."
+          />
+        </StyledSelectArea>
         <div>
-          {data.allMdx.edges
-            .filter(nodeObj => filterByYear(nodeObj, yearOption))
-            .map(({ node }, index) => (
+          {matchingStories.length ? (
+            matchingStories.map(({ node }, index) => (
               <StoryCard
                 caption={node.frontmatter.caption}
                 date={node.frontmatter.date}
@@ -69,7 +117,12 @@ const Stories = ({ data, location }) => {
                 title={node.frontmatter.title}
                 timeToRead={node.timeToRead}
               />
-            ))}
+            ))
+          ) : (
+            <p>
+              No stories match your search. Try removing some of your filters.
+            </p>
+          )}
         </div>
       </StyledStoriesWrapper>
     </MainLayout>
@@ -82,8 +135,8 @@ export const query = graphql`
   query StoriesQuery {
     allMdx(
       sort: {
-        fields: [frontmatter___date, frontmatter___tags]
-        order: [DESC, ASC]
+        fields: frontmatter___date
+        order: DESC
       }
     ) {
       edges {
