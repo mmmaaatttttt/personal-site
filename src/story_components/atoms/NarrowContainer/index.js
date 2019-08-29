a6b6c3a7f468041e5a8c1123340812e5959e2f19
Matt Lane <lane.matthew@gmail.com>
2019-08-29 08:22:49 -0700

    NarrowContainers can have centered text

--- src/story_components/atoms/NarrowContainer/index.js
+++ src/story_components/atoms/NarrowContainer/index.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
 import media, { sizes } from "utils/media";
 
@@ -10,15 +10,21 @@ const NarrowContainer = styled.div`
     width: 100%;
     margin: 0 auto;
   `};
+
+  ${props => props.center && css`
+    text-align: center;
+  `}
 `;
 
 NarrowContainer.propTypes = {
+  center: PropTypes.bool.isRequired,
   fullWidthAt: PropTypes.oneOf(Object.keys(sizes)),
   margin: PropTypes.string.isRequired,
   width: PropTypes.string.isRequired
 };
 
 NarrowContainer.defaultProps = {
+  center: false,
   fullWidthAt: "extraSmall",
   margin: "0 auto",
   width: "80%"
