94191603d2dde71a2a72adb1b57dc71ec4aa3b0a
Matt Lane <lane.matthew@gmail.com>
2020-04-27 20:43:57 -0700

    making switch from enzyme to rtl

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/test/__mocks__/fileMock.js",
   },
+  setupFilesAfterEnv: ["<rootDir>/test/setup-test-env.js"],
   setupTestFrameworkScriptFile: "<rootDir>/test/jest-setup.js",
   snapshotSerializers: ["enzyme-to-json/serializer"],
   testPathIgnorePatterns: ["/node_modules/", "<rootDir>/.cache/"],
