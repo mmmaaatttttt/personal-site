78912de21815cb5baa9468b1362704b4911dc02b
Matt Lane <lane.matthew@gmail.com>
2020-05-04 14:13:36 -0700

    update CoinFlipTable to function component

--- src/story_components/molecules/CoinFlipTable/index.js
+++ src/story_components/molecules/CoinFlipTable/index.js
@@ -1,78 +1,63 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { ColoredSpan, LabeledSlider, StyledTable } from "story_components";
 import { withCaption } from "providers";
 import COLORS from "utils/styles";
 
-class CoinFlipTable extends Component {
-  state = {
-    headsProb: 0.5
-  };
+const format = (prob, dec = 0) => `${(prob * 100).toFixed(dec)}%`;
 
-  handleProbChange = val => {
-    this.setState({ headsProb: val });
-  };
+function CoinFlipTable() {
+  const [headsProb, setHeadsProb] = useState(0.5);
+  const tailsProb = 1 - headsProb;
+  const pairProb = headsProb * tailsProb;
+  const headsFormat = format(headsProb);
+  const tailsFormat = format(tailsProb);
+  const pairFormat = format(pairProb, 2);
 
-  format = (probability, dec = 0) => `${(probability * 100).toFixed(dec)}%`;
-
-  render() {
-    const { headsProb } = this.state;
-    const tailsProb = 1 - headsProb;
-    const pairProb = headsProb * tailsProb;
-    const formatted = {
-      heads: this.format(headsProb),
-      tails: this.format(tailsProb),
-      pair: this.format(pairProb, 2)
-    };
-    return (
-      <div>
-        <LabeledSlider
-          min={0.01}
-          max={0.99}
-          step={0.01}
-          value={headsProb}
-          handleValueChange={this.handleProbChange}
-          title={`Probability of flipping heads: ${formatted.heads}`}
-          color={COLORS.GREEN}
-          minIcon="times-circle"
-          maxIcon="check-circle"
-        />
-        <StyledTable margin="0.72rem 0 0 0">
-          <thead>
-            <tr>
-              <th>Prob. of H</th>
-              <th>Prob. of T</th>
-              <th>Prob. of HT</th>
-              <th>Prob. of TH</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{formatted.heads}</td>
-              <td>{formatted.tails}</td>
-              <td>
-                {formatted.heads} &times; {formatted.tails} ={" "}
-                <ColoredSpan color={COLORS.GREEN} bold>
-                  {formatted.pair}
-                </ColoredSpan>
-              </td>
-              <td>
-                {formatted.tails} &times; {formatted.heads} ={" "}
-                <ColoredSpan color={COLORS.GREEN} bold>
-                  {formatted.pair}
-                </ColoredSpan>
-              </td>
-            </tr>
-          </tbody>
-        </StyledTable>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <LabeledSlider
+        min={0.01}
+        max={0.99}
+        step={0.01}
+        value={headsProb}
+        handleValueChange={setHeadsProb}
+        title={`Probability of flipping heads: ${headsFormat}`}
+        color={COLORS.GREEN}
+        minIcon="times-circle"
+        maxIcon="check-circle"
+      />
+      <StyledTable margin="0.72rem 0 0 0">
+        <thead>
+          <tr>
+            <th>Prob. of H</th>
+            <th>Prob. of T</th>
+            <th>Prob. of HT</th>
+            <th>Prob. of TH</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{headsFormat}</td>
+            <td>{tailsFormat}</td>
+            <td>
+              {headsFormat} &times; {tailsFormat} ={" "}
+              <ColoredSpan color={COLORS.GREEN} bold>
+                {pairFormat}
+              </ColoredSpan>
+            </td>
+            <td>
+              {tailsFormat} &times; {headsFormat} ={" "}
+              <ColoredSpan color={COLORS.GREEN} bold>
+                {pairFormat}
+              </ColoredSpan>
+            </td>
+          </tr>
+        </tbody>
+      </StyledTable>
+    </div>
+  );
 }
 
-CoinFlipTable.propTypes = {};
-
-CoinFlipTable.defaultProps = {};
-
-export default withCaption(CoinFlipTable);
+export default React.memo(withCaption(CoinFlipTable));
 
 export { CoinFlipTable };
