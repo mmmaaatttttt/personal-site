b08f9eb3aec63ba53140efbeace656b24c46285f
Matt Lane <lane.matthew@gmail.com>
2020-05-06 13:06:36 -0700

    update LabeledCircle to function component

--- src/story_components/molecules/LabeledCircle/index.js
+++ src/story_components/molecules/LabeledCircle/index.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import styled, { css, keyframes } from "styled-components";
 import PropTypes from "prop-types";
 import { CenteredSVGText } from "story_components";
 import COLORS from "utils/styles";
+import { noop } from "utils/fnHelpers";
 
 // pulse inspiration:
 // https://codepen.io/riccardoscalco/pen/GZzZRz
@@ -25,64 +26,47 @@ const StyledCircle = styled.circle`
       animation: ${pulse(props)} 2s infinite;
     `}
 `;
-class LabeledCircle extends Component {
-  render() {
-    const {
-      x,
-      y,
-      color,
-      label,
-      isActive,
-      r,
-      handleLeave,
-      handleUpdate
-    } = this.props;
-    return (
-      <g
-        onMouseMove={handleUpdate}
-        onTouchMove={handleUpdate}
-        onMouseLeave={handleLeave}
-        onTouchEnd={handleLeave}
-      >
-        <StyledCircle
-          isActive={isActive}
-          cx={x}
-          cy={y}
-          fill={color}
-          r={r}
-          stroke={color}
-        />
-        <CenteredSVGText
-          x={x}
-          y={y}
-          fontSize={`${1.5 * r}px`}
-          baseline="central"
-        >
-          {label}
-        </CenteredSVGText>
-      </g>
-    );
-  }
+function LabeledCircle({
+  color = COLORS.DARK_GRAY,
+  handleLeave = noop,
+  handleUpdate = noop,
+  isActive = false,
+  label = "",
+  x = 0,
+  y = 0,
+  r = 10
+}) {
+  return (
+    <g
+      onMouseMove={handleUpdate}
+      onTouchMove={handleUpdate}
+      onMouseLeave={handleLeave}
+      onTouchEnd={handleLeave}
+    >
+      <StyledCircle
+        isActive={isActive}
+        cx={x}
+        cy={y}
+        fill={color}
+        r={r}
+        stroke={color}
+      />
+      <CenteredSVGText x={x} y={y} fontSize={`${1.5 * r}px`} baseline="central">
+        {label}
+      </CenteredSVGText>
+    </g>
+  );
 }
 
 LabeledCircle.propTypes = {
-  color: PropTypes.string.isRequired,
+  color: PropTypes.string,
   handleLeave: PropTypes.func,
   handleUpdate: PropTypes.func,
-  isActive: PropTypes.bool.isRequired,
-  label: PropTypes.string.isRequired,
-  r: PropTypes.number.isRequired,
-  x: PropTypes.number.isRequired,
-  y: PropTypes.number.isRequired
+  isActive: PropTypes.bool,
+  label: PropTypes.string,
+  r: PropTypes.number,
+  x: PropTypes.number,
+  y: PropTypes.number
 };
 
-LabeledCircle.defaultProps = {
-  color: COLORS.DARK_GRAY,
-  isActive: false,
-  label: "",
-  x: 0,
-  y: 0,
-  r: 10
-};
-
-export default LabeledCircle;
+export default React.memo(LabeledCircle);
