cb26f82512ca1dbab6a8c59e25b9722b9ba6f0c6
Matt Lane <lane.matthew@gmail.com>
2020-05-06 12:59:34 -0700

    update InteractivePolygon to function component

--- src/story_components/molecules/InteractivePolygon/index.js
+++ src/story_components/molecules/InteractivePolygon/index.js
@@ -1,70 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { DraggableCircle, Polygon } from "story_components";
 import COLORS from "utils/styles";
 
-class InteractivePolygon extends Component {
-  render() {
-    const { circleRadius, fill, points, stroke, strokeWidth } = this.props;
-    let circles = points.map((point, i) => (
-      <DraggableCircle
-        cx={point.x}
-        cy={point.y}
-        fill={fill}
-        key={i}
-        id={i}
-        onDrag={this.props.handleDrag}
-        r={circleRadius}
+const defaultHandleDrag = (i, coords) => console.log(i, coords);
+const defaultPts = [];
+
+function InteractivePolygon({
+  circleRadius = 8,
+  handleDrag = defaultHandleDrag,
+  fill = COLORS.BLACK,
+  points = defaultPts,
+  stroke = COLORS.BLACK,
+  strokeWidth = 3
+}) {
+  let circles = points.map((point, i) => (
+    <DraggableCircle
+      cx={point.x}
+      cy={point.y}
+      fill={fill}
+      key={i}
+      id={i}
+      onDrag={handleDrag}
+      r={circleRadius}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+    />
+  ));
+  let lines = points.map((point, i) => {
+    let nextPoint = points[(i + 1) % points.length];
+    return (
+      <line
+        x1={point.x}
+        x2={nextPoint.x}
+        y1={point.y}
+        y2={nextPoint.y}
         stroke={stroke}
         strokeWidth={strokeWidth}
+        key={`${i}-${i + 1}`}
       />
-    ));
-    let lines = points.map((point, i) => {
-      let nextPoint = points[(i + 1) % points.length];
-      return (
-        <line
-          x1={point.x}
-          x2={nextPoint.x}
-          y1={point.y}
-          y2={nextPoint.y}
-          stroke={stroke}
-          strokeWidth={strokeWidth}
-          key={`${i}-${i + 1}`}
-        />
-      );
-    });
-    let polygon = <Polygon points={points} fill={fill} stroke="none" />;
-    return (
-      <g>
-        {polygon}
-        {lines}
-        {circles}
-      </g>
     );
-  }
+  });
+  let polygon = <Polygon points={points} fill={fill} stroke="none" />;
+  return (
+    <g>
+      {polygon}
+      {lines}
+      {circles}
+    </g>
+  );
 }
 
 InteractivePolygon.propTypes = {
-  circleRadius: PropTypes.number.isRequired,
-  handleDrag: PropTypes.func.isRequired,
-  fill: PropTypes.string.isRequired,
+  circleRadius: PropTypes.number,
+  handleDrag: PropTypes.func,
+  fill: PropTypes.string,
   points: PropTypes.arrayOf(
     PropTypes.shape({
-      x: PropTypes.number.isRequired,
-      y: PropTypes.number.isRequired
+      x: PropTypes.number,
+      y: PropTypes.number
     })
-  ).isRequired,
-  stroke: PropTypes.string.isRequired,
-  strokeWidth: PropTypes.number.isRequired
+  ),
+  stroke: PropTypes.string,
+  strokeWidth: PropTypes.number
 };
 
-InteractivePolygon.defaultProps = {
-  circleRadius: 8,
-  handleDrag: (i, coords) => console.log(i, coords),
-  fill: COLORS.BLACK,
-  points: [],
-  stroke: COLORS.BLACK,
-  strokeWidth: 3
-};
-
-export default InteractivePolygon;
+export default React.memo(InteractivePolygon);
