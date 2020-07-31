14423659b3e936d97c445b4d8468c6f72d5b26fc
Matt Lane <lane.matthew@gmail.com>
2020-07-31 12:20:57 -0700

    MainLayout to function component

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { StaticQuery, graphql } from "gatsby";
 import styled from "styled-components";
 import { Helmet } from "react-helmet";
@@ -23,54 +23,45 @@ const StyledPageWrapper = styled.div`
   flex-direction: column;
 `;
 
-class MainLayout extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { show: false };
-  }
+function MainLayout({ children, location, outline }) {
+  const [show, setShow] = useState(false);
 
-  componentDidMount() {
-    this.setState({ show: true });
-  }
+  useEffect(() => {
+    setShow(true);
+  }, []);
 
-  render() {
-    const { children, location, outline } = this.props;
-    const display = this.state.show ? "flex" : "none";
-    return (
-      <StaticQuery
-        query={query}
-        render={data => {
-          const { title, description, siteUrl } = data.site.siteMetadata;
-          return (
-            <StyledPageWrapper style={{ display }}>
-              <Helmet>
-                <title>{title}</title>
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="@mmmaaatttttt" />
-                <meta name="twitter:title" content={title} />
-                <meta name="twitter:description" content={description} />
-                <meta name="twitter:image" content={`${siteUrl}${matt}`} />
-                <meta name="og:title" content={title} />
-                <meta name="og:description" content={description} />
-                <meta
-                  name="og:url"
-                  content={`${siteUrl}${location.pathname}`}
-                />
-                <meta name="og:image" content={`${siteUrl}${matt}`} />
-              </Helmet>
-              <Navbar
-                title={title}
-                hide={/\/stories\/.+/.test(location.pathname)}
-                outline={outline}
-              />
-              <StyledContentArea>{children}</StyledContentArea>
-              <Footer showMailingListForm={location.pathname !== "/"} />
-            </StyledPageWrapper>
-          );
-        }}
-      />
-    );
-  }
+  const display = show ? "flex" : "none";
+  return (
+    <StaticQuery
+      query={query}
+      render={data => {
+        const { title, description, siteUrl } = data.site.siteMetadata;
+        return (
+          <StyledPageWrapper style={{ display }}>
+            <Helmet>
+              <title>{title}</title>
+              <meta name="twitter:card" content="summary_large_image" />
+              <meta name="twitter:site" content="@mmmaaatttttt" />
+              <meta name="twitter:title" content={title} />
+              <meta name="twitter:description" content={description} />
+              <meta name="twitter:image" content={`${siteUrl}${matt}`} />
+              <meta name="og:title" content={title} />
+              <meta name="og:description" content={description} />
+              <meta name="og:url" content={`${siteUrl}${location.pathname}`} />
+              <meta name="og:image" content={`${siteUrl}${matt}`} />
+            </Helmet>
+            <Navbar
+              title={title}
+              hide={/\/stories\/.+/.test(location.pathname)}
+              outline={outline}
+            />
+            <StyledContentArea>{children}</StyledContentArea>
+            <Footer showMailingListForm={location.pathname !== "/"} />
+          </StyledPageWrapper>
+        );
+      }}
+    />
+  );
 }
 
 export default MainLayout;
