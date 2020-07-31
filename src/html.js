742913fc7851885a959356eda4c94d93915cef43
Matt Lane <lane.matthew@gmail.com>
2020-07-31 12:18:29 -0700

    html.js to function component

--- src/html.js
+++ src/html.js
@@ -1,33 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import favicon from "./favicon.ico";
 
-class HTML extends Component {
-  render() {
-    return (
-      <html {...this.props.htmlAttributes} lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <link rel="shortcut icon" href={favicon} />
-          {this.props.headComponents}
-          {/* {css} */}
-        </head>
-        <body {...this.props.bodyAttributes}>
-          {this.props.preBodyComponents}
-          <div
-            key="body"
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
-          />
-          {this.props.postBodyComponents}
-        </body>
-      </html>
-    );
-  }
-};
+function HTML() {
+  return (
+    <html {...this.props.htmlAttributes} lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link rel="shortcut icon" href={favicon} />
+        {this.props.headComponents}
+        {/* {css} */}
+      </head>
+      <body {...this.props.bodyAttributes}>
+        {this.props.preBodyComponents}
+        <div
+          key="body"
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: this.props.body }}
+        />
+        {this.props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
 
 export default HTML;
