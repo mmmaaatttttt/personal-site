ec766255bb517eed2e3c5bdab4611759969d8f7c
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-04-12 13:06:10 -0700

    update CollocationTable

---
+++ content/stories/beautiful-analysis/components/CollocationTable/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+import ColoredSpan from "@/components/story/shared/ColoredSpan";
+import StyledTable from "@/components/story/shared/StyledTable";
+import NarrowContainer from "@/components/story/shared/NarrowContainer";
+import baCommonPhrases from "../../data/ba-common-phrases.json";
+import { colorMap } from "../../data/beautiful-analysis";
+
+const CollocationTable = () => {
+  const headers = baCommonPhrases.collocation_lists.map(({ speaker }) => ({
+    key: `header-${speaker}`,
+    content: (
+      <ColoredSpan bold color={colorMap[speaker as keyof typeof colorMap]}>
+        {speaker}
+      </ColoredSpan>
+    )
+  }));
+
+  const rows = baCommonPhrases.collocation_lists[0].collocations.map(
+    (phrase, idx) => ({
+      key: `${phrase}-${baCommonPhrases.collocation_lists[1].collocations[idx]}`,
+      cells: [
+        {
+          key: "chris-collocation",
+          content: (
+            <ColoredSpan color={colorMap["Chris"]}>
+              {phrase}
+            </ColoredSpan>
+          )
+        },
+        {
+          key: "caller-collocation",
+          content: (
+            <ColoredSpan color={colorMap["Caller"]}>
+              {baCommonPhrases.collocation_lists[1].collocations[idx]}
+            </ColoredSpan>
+          )
+        }
+      ]
+    })
+  );
+
+  return (
+    <div className="w-full my-12" data-testid="collocation-table-container">
+      <NarrowContainer width="70%" fullWidthAt="sm">
+        <StyledTable padding="0.1rem" headers={headers} rows={rows} />
+      </NarrowContainer>
+    </div>
+  );
+};
+
+export default CollocationTable;
