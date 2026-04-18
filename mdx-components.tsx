f94c52e55cf0cb31fb0622d9966550ff3eb8ce33
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-04-18 15:18:25 -0700

    add mdx-components file

---
+++ mdx-components.tsx
@@ -0,0 +1,10 @@
+import { MdxComponents } from "@/components/mdx/MdxComponents";
+
+export function useMDXComponents(
+  components: Record<string, any>
+): Record<string, any> {
+  return {
+    ...MdxComponents,
+    ...components,
+  };
+}
