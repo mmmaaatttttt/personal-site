98d76acb8f25584afdb406f9ab82e564d56debd5
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-04-29 07:33:26 -0700

    custom hook

---
+++ content/stories/keeping-distances/components/DistanceExplorer/useDragState.ts
@@ -0,0 +1,21 @@
+import { useState, useCallback } from "react";
+import { ScaleLinear } from "d3-scale";
+
+function useDragState(
+  initialState: { x: number; y: number }[],
+  xScale: ScaleLinear<number, number>,
+  yScale: ScaleLinear<number, number>
+): [{ x: number; y: number }[], (idx: number, coords: { x: number; y: number }) => void] {
+  const [points, setPoints] = useState(initialState);
+  const handleDrag = useCallback(
+    (idx: number, { x, y }: { x: number; y: number }) => {
+      const pointsCopy = [...points];
+      pointsCopy[idx] = { x: xScale.invert(x), y: yScale.invert(y) };
+      setPoints(pointsCopy);
+    },
+    [points, xScale, yScale]
+  );
+  return [points, handleDrag];
+}
+
+export default useDragState;
