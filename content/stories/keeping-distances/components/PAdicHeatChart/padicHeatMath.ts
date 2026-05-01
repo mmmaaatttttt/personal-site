da3fcf270507ce101c7792137ab23b92ac415711
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-04-30 20:15:36 -0700

    padic heat math

---
+++ content/stories/keeping-distances/components/PAdicHeatChart/padicHeatMath.ts
@@ -0,0 +1,18 @@
+function pAdicNorm(num: number, p: number): number {
+  let absN = Math.abs(num);
+  let power = 0;
+  while (absN % p === 0 && absN > 1) {
+    power++;
+    absN /= p;
+  }
+  return p ** -power;
+}
+
+export function generateGrid(size: number, prime: number): (number | null)[][] {
+  return Array.from({ length: size }, (_, yIdx) =>
+    Array.from({ length: size }, (_, xIdx) => {
+      if (yIdx < xIdx) return null;
+      return pAdicNorm(xIdx - yIdx, prime);
+    })
+  );
+}
