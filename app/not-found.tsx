0947457ed474bd28f9094261488a397ffd6b1730
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-05-07 20:24:27 -0700

    fix import path

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
+import Image from "next/image";
 import Link from "next/link";
-import Image from "next-export-optimize-images/image";
 import MainLayout from "@/components/layout/MainLayout";
 import { storyMeta } from "@/utils/storyMeta";
 import { normalizeImagePath } from "@/utils/stringHelpers";
