c0a75f41425d129ff8af1e791da0c1c694d1dcf6
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-05-11 19:10:53 -0700

    add e2e spec

---
+++ e2e/fairest-of-them-all.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "@playwright/test";
+
+test("fairest-of-them-all story page", async ({ page }) => {
+  await page.goto("/stories/fairest-of-them-all");
+  await page.getByRole("heading", { level: 1 }).waitFor({ state: "visible" });
+  await page.waitForLoadState("networkidle");
+  await expect(page).toHaveScreenshot("fairest-of-them-all.png", {
+    fullPage: true,
+  });
+});
