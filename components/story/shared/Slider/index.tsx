bab80664489aff9dc5e5b34a4928dcacc982fd08
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-05-27 22:28:57 -0700

    remove slider provider

--- components/story/shared/Slider/index.tsx
+++ components/story/shared/Slider/index.tsx
@@ -1,8 +1,5 @@
 import LabeledSlider from "./LabeledSlider";
 import Slider from "./Slider";
 import SliderGroup from "./SliderGroup";
-import SliderProvider from "./SliderProvider";
 
 export { LabeledSlider, Slider, SliderGroup };
-
-export default SliderProvider;
