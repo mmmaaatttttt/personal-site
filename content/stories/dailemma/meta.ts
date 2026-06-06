6a553634634de6220b158914dff7401e9ff7f3ac
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-06-06 11:47:28 -0700

    add story draft and meta

--- content/stories/dailemma/meta.ts
+++ content/stories/dailemma/meta.ts
@@ -1,11 +1,13 @@
 import type { ArticleFrontmatter } from "@/utils/content";
 
 const meta: ArticleFrontmatter = {
-  title: "d-AI-lemma",
-  date: "2026-05-26",
+  title: "d-AI-lemma (Part 1)",
+  date: "2026-06-18",
   featured_image: "../../images/featured_images/dailemma.jpg",
-  caption: "tbd",
-  featured_image_caption: "tbd",
+  caption:
+    "How firms who rush to embrace AI may be hurting themselves (and the rest of us) in the long run.",
+  featured_image_caption:
+    "Photo by [Emilipothèse](https://unsplash.com/@emilipothese?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) on [Unsplash](https://unsplash.com/photos/blue-plastic-robot-toy-R4WCbazrD1g?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)",
   tags: ["game theory", "economics", "AI", "labor", "prisoner's dilemma"],
 };
 
