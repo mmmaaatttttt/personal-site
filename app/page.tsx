d083cc46faca6f022abc3540b784ae96d61204dc
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-06-11 22:12:43 -0700

    faster homepage

--- app/page.tsx
+++ app/page.tsx
@@ -23,16 +23,16 @@ export default function Home() {
       <script type="application/ld+json">{JSON.stringify(jsonLd)}</script>
       <div className="flex-1 flex flex-col items-center justify-evenly text-center opacity-0 animate-[fade-in_2s_ease-out_0.5s_forwards]">
         <h1 className="font-serif text-[5rem] font-black leading-none">Hi!</h1>
-        <h2 className="font-serif text-4xl font-bold leading-tight opacity-0 animate-[fade-in_1s_ease-out_1.5s_forwards]">
+        <h2 className="font-serif text-4xl font-bold leading-tight opacity-0 animate-[fade-in_1s_ease-out_1s_forwards]">
           I&apos;m Matt.{" "}
           <span role="img" aria-label="wave">
             👋
           </span>
         </h2>
-        <p className="max-w-md opacity-0 animate-[fade-in_1s_ease-out_2.5s_forwards]">
+        <p className="max-w-md opacity-0 animate-[fade-in_1s_ease-out_1.5s_forwards]">
           Use the nav bar to explore the site. You&apos;ll figure it out.
         </p>
-        <div className="opacity-0 animate-[fade-in_1s_ease-out_3.5s_forwards]">
+        <div className="opacity-0 animate-[fade-in_1s_ease-out_2s_forwards]">
           <p className="text-sm mb-3">
             If you like, you can check out my latest story:
           </p>
