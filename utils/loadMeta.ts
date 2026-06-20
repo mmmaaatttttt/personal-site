5b65adf22fb4a3f548d3f9d73c86cc25c5cf826d
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-06-19 18:14:55 -0700

    explicit file extension for dynamic import

--- utils/loadMeta.ts
+++ utils/loadMeta.ts
@@ -1,6 +1,6 @@
 import type { ArticleFrontmatter } from "@/utils/content";
 
 export async function loadMeta(slug: string): Promise<ArticleFrontmatter> {
-  const mod = await import(`@/content/stories/${slug}/meta`);
+  const mod = await import(`@/content/stories/${slug}/meta.ts`);
   return mod.default as ArticleFrontmatter;
 }
