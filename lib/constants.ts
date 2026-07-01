7d50a8c152acdb788f5f2c7d9649f760dc6efd84
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-07-01 14:00:12 -0700

    constants + types

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,10 @@
 export const SITE_URL = "https://mattlane.us";
 export const SITE_DOMAIN = new URL(SITE_URL).hostname;
+export const EMAIL_SUBSCRIBED_STORAGE_KEY = "email-subscribed";
+export const EMAIL_SIGNUP_DISMISSED_STORAGE_KEY = "email-signup-dismissed-at";
+export const EMAIL_SIGNUP_DISMISS_COOLDOWN_MS = 7 * 24 * 60 * 60 * 1000;
+
+export const EMAIL_SIGNUP_VIEW_EVENT = "email-signup-view";
+export const EMAIL_SIGNUP_SUBMIT_SUCCESS_EVENT = "email-signup-submit-success";
+export const EMAIL_SIGNUP_SUBMIT_ERROR_EVENT = "email-signup-submit-error";
+export const EMAIL_SIGNUP_DISMISSED_EVENT = "email-signup-dismissed";
