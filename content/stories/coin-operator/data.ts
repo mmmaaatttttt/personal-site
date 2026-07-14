cf6799ad2a0f2a5db3e95b2268a34915e9c80c09
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-07-14 11:39:13 -0700

    add classifications

--- content/stories/coin-operator/data.ts
+++ content/stories/coin-operator/data.ts
@@ -44,7 +44,7 @@ export const SYMBOL_EMOJI: Record<SlotValue, string> = {
   [SlotValue.SNAKE]: "🐍",
 };
 
-const SYMBOL_NAME: Record<SlotValue, string> = {
+export const SYMBOL_NAME: Record<SlotValue, string> = {
   [SlotValue.CLOVER]: "Clover",
   [SlotValue.COIN_1]: "Coin",
   [SlotValue.COIN_3]: "3 Coins",
@@ -55,17 +55,72 @@ const SYMBOL_NAME: Record<SlotValue, string> = {
   [SlotValue.SNAKE]: "Snake",
 };
 
-const probabilityData: [string, string, number][] = (
-  Object.values(SlotValue) as SlotValue[]
-).map((symbol) => [
-  SYMBOL_EMOJI[symbol],
-  SYMBOL_NAME[symbol],
-  PROBABILITY_MAP[symbol],
-]);
+export const PayoutClassifications = {
+  COIN_1_3: SYMBOL_EMOJI[SlotValue.COIN_1].repeat(3),
+  COIN_1_4: SYMBOL_EMOJI[SlotValue.COIN_1].repeat(4),
+  COIN_1_3_DOUBLE_1:
+    SYMBOL_EMOJI[SlotValue.COIN_1].repeat(3) + SYMBOL_EMOJI[SlotValue.DOUBLE],
+  COIN_3_3: SYMBOL_EMOJI[SlotValue.COIN_3].repeat(3),
+  COIN_3_4: SYMBOL_EMOJI[SlotValue.COIN_3].repeat(4),
+  COIN_3_3_DOUBLE_1:
+    SYMBOL_EMOJI[SlotValue.COIN_3].repeat(3) + SYMBOL_EMOJI[SlotValue.DOUBLE],
+  SNAKE_1_NET: SYMBOL_EMOJI[SlotValue.SNAKE] + SYMBOL_EMOJI[SlotValue.NET],
+  SNAKE_2_NET:
+    SYMBOL_EMOJI[SlotValue.SNAKE].repeat(2) + SYMBOL_EMOJI[SlotValue.NET],
+  SNAKE_3_NET:
+    SYMBOL_EMOJI[SlotValue.SNAKE].repeat(3) + SYMBOL_EMOJI[SlotValue.NET],
+  SNAKE_1_DOUBLE_1_NET:
+    SYMBOL_EMOJI[SlotValue.SNAKE] +
+    SYMBOL_EMOJI[SlotValue.DOUBLE] +
+    SYMBOL_EMOJI[SlotValue.NET],
+  SNAKE_1_DOUBLE_2_NET:
+    SYMBOL_EMOJI[SlotValue.SNAKE] +
+    SYMBOL_EMOJI[SlotValue.DOUBLE].repeat(2) +
+    SYMBOL_EMOJI[SlotValue.NET],
+  SNAKE_2_DOUBLE_1_NET:
+    SYMBOL_EMOJI[SlotValue.SNAKE].repeat(2) +
+    SYMBOL_EMOJI[SlotValue.DOUBLE] +
+    SYMBOL_EMOJI[SlotValue.NET],
+  CROWN_4: SYMBOL_EMOJI[SlotValue.CROWN].repeat(4),
+  COIN_1_3_CLOVER_1:
+    SYMBOL_EMOJI[SlotValue.COIN_1].repeat(3) + SYMBOL_EMOJI[SlotValue.CLOVER],
+  COIN_3_3_CLOVER_1:
+    SYMBOL_EMOJI[SlotValue.COIN_3].repeat(3) + SYMBOL_EMOJI[SlotValue.CLOVER],
+  SNAKE_1_CLOVER_1_NET:
+    SYMBOL_EMOJI[SlotValue.SNAKE] +
+    SYMBOL_EMOJI[SlotValue.CLOVER] +
+    SYMBOL_EMOJI[SlotValue.NET],
+  SNAKE_1_CLOVER_2_NET:
+    SYMBOL_EMOJI[SlotValue.SNAKE] +
+    SYMBOL_EMOJI[SlotValue.CLOVER].repeat(2) +
+    SYMBOL_EMOJI[SlotValue.NET],
+  SNAKE_2_CLOVER_1_NET:
+    SYMBOL_EMOJI[SlotValue.SNAKE].repeat(2) +
+    SYMBOL_EMOJI[SlotValue.CLOVER] +
+    SYMBOL_EMOJI[SlotValue.NET],
+  SNAKE_1_CLOVER_1_DOUBLE_1_NET:
+    SYMBOL_EMOJI[SlotValue.SNAKE] +
+    SYMBOL_EMOJI[SlotValue.CLOVER] +
+    SYMBOL_EMOJI[SlotValue.DOUBLE] +
+    SYMBOL_EMOJI[SlotValue.NET],
+  CLOVER_1: SYMBOL_EMOJI[SlotValue.CLOVER],
+  CLOVER_2: SYMBOL_EMOJI[SlotValue.CLOVER].repeat(2),
+  CLOVER_3: SYMBOL_EMOJI[SlotValue.CLOVER].repeat(3),
+  CLOVER_4: SYMBOL_EMOJI[SlotValue.CLOVER].repeat(4),
+  CLOVER_1_DOUBLE_1:
+    SYMBOL_EMOJI[SlotValue.CLOVER] + SYMBOL_EMOJI[SlotValue.DOUBLE],
+  CLOVER_1_DOUBLE_2:
+    SYMBOL_EMOJI[SlotValue.CLOVER] + SYMBOL_EMOJI[SlotValue.DOUBLE].repeat(2),
+  CLOVER_1_DOUBLE_3:
+    SYMBOL_EMOJI[SlotValue.CLOVER] + SYMBOL_EMOJI[SlotValue.DOUBLE].repeat(3),
+  CLOVER_2_DOUBLE_1:
+    SYMBOL_EMOJI[SlotValue.CLOVER].repeat(2) + SYMBOL_EMOJI[SlotValue.DOUBLE],
+  CLOVER_2_DOUBLE_2:
+    SYMBOL_EMOJI[SlotValue.CLOVER].repeat(2) +
+    SYMBOL_EMOJI[SlotValue.DOUBLE].repeat(2),
+  CLOVER_3_DOUBLE_1:
+    SYMBOL_EMOJI[SlotValue.CLOVER].repeat(3) + SYMBOL_EMOJI[SlotValue.DOUBLE],
+} as const;
 
-export const probabilityTable: string[][] = [
-  ["Symbol", "Symbol Name", "Probability"],
-  ...probabilityData
-    .sort((row1, row2) => row2[2] - row1[2])
-    .map((row) => [row[0], row[1], `${(row[2] * 100).toFixed(1)}%`]),
-];
+export type PayoutClassificationValue =
+  (typeof PayoutClassifications)[keyof typeof PayoutClassifications];
