dab94de961915f338e96acfcb51eee35c0d06175
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-08-04 18:01:42 -0700

    wip story start

---
+++ content/stories/bettor-up/meta.ts
@@ -0,0 +1,12 @@
+import type { ArticleFrontmatter } from "@/utils/content";
+
+const meta: ArticleFrontmatter = {
+  title: "Bettor Up",
+  date: "2026-08-10",
+  featured_image: "../../images/featured_images/jane_the_virgin.jpg",
+  caption: "TBD",
+  featured_image_caption: "TBD",
+  tags: ["TBD"],
+};
+
+export default meta;
