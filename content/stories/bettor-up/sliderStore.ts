fd626feca31bf52a91e415a31d11c17d4e6c3363
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-08-16 15:36:20 -0700

    attrition bias hooks

---
+++ content/stories/bettor-up/sliderStore.ts
@@ -0,0 +1,7 @@
+export const TRUE_RATE_KEY = "bettor-up-trueRate";
+export const RESPONDER_COMPLETION_KEY = "bettor-up-responderCompletion";
+export const NON_RESPONDER_COMPLETION_KEY = "bettor-up-nonResponderCompletion";
+
+export const DEFAULT_TRUE_RATE = 0.5;
+export const DEFAULT_RESPONDER_COMPLETION = 1;
+export const DEFAULT_NON_RESPONDER_COMPLETION = 1;
