6c76848ad1955cbb01aa9734939dbf8dacb13721
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2026-08-22 12:11:52 -0700

    add images, update meta

--- content/stories/bettor-up/meta.ts
+++ content/stories/bettor-up/meta.ts
@@ -2,11 +2,19 @@ import type { ArticleFrontmatter } from "@/utils/content";
 
 const meta: ArticleFrontmatter = {
   title: "Bettor Up",
-  date: "2026-08-10",
-  featured_image: "../../images/featured_images/jane_the_virgin.jpg",
-  caption: "TBD",
-  featured_image_caption: "TBD",
-  tags: ["TBD"],
+  date: "2026-08-24",
+  featured_image: "../../images/featured_images/bettor_up.jpg",
+  caption:
+    "When medical trials get turbo charged with even more capitalism, what could possibly go wrong?",
+  featured_image_caption:
+    "This doctor wants prediction markets even more than he wants to keep money out of healthcare. IYKYK.",
+  tags: [
+    "expected value",
+    "probability",
+    "functions",
+    "healthcare",
+    "economics",
+  ],
 };
 
 export default meta;
